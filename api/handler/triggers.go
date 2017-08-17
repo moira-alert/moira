fd112ce806cbdcd395c4488152b000dcbc3a643a
borovskyav <borovskyav@skbkontur.ru>
2017-08-17 12:58:07 +0500

    triggers now can filter in url

--- api/handler/triggers.go
+++ api/handler/triggers.go
@@ -1,11 +1,13 @@
 package handler
 
 import (
+	"fmt"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/render"
 	"github.com/moira-alert/moira-alert/api/controller"
 	"github.com/moira-alert/moira-alert/api/dto"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
@@ -48,27 +50,8 @@ func createTrigger(writer http.ResponseWriter, request *http.Request) {
 }
 
 func getTriggersPage(writer http.ResponseWriter, request *http.Request) {
-	var onlyErrors bool
-	var filterTags []string
-
-	filterOkCookie, err := request.Cookie("moira_filter_ok")
-	if err == http.ErrNoCookie {
-		onlyErrors = false
-	} else {
-		onlyErrors = filterOkCookie.Value == "true"
-	}
-
-	fillerTagsCookie, err := request.Cookie("moira_filter_tags")
-	if err == http.ErrNoCookie {
-		filterTags = make([]string, 0)
-	} else {
-		str := strings.Split(fillerTagsCookie.Value, ",")
-		for _, tag := range str {
-			if tag != "" {
-				filterTags = append(filterTags, tag)
-			}
-		}
-	}
+	onlyErrors := getFilterOkFlag(request)
+	filterTags := getRequestTags(request)
 
 	page := request.Context().Value("page").(int64)
 	size := request.Context().Value("size").(int64)
@@ -82,5 +65,51 @@ func getTriggersPage(writer http.ResponseWriter, request *http.Request) {
 	if err := render.Render(writer, request, triggersList); err != nil {
 		render.Render(writer, request, dto.ErrorRender(err))
 		return
+	}
+}
+
+func getRequestTags(request *http.Request) []string {
+	var filterTags []string
+	request.ParseForm()
+	i := 0
+	for {
+		tag := request.FormValue(fmt.Sprintf("tags[%v]", i))
+		if tag == "" {
+			break
+		}
+		filterTags = append(filterTags, tag)
+		i++
+	}
+
+	if len(filterTags) != 0 {
+		return filterTags
+	}
+
+	fillerTagsCookie, err := request.Cookie("moira_filter_tags")
+	if err == http.ErrNoCookie {
+		filterTags = make([]string, 0)
+	} else {
+		str := strings.Split(fillerTagsCookie.Value, "%2C")
+		for _, tag := range str {
+			if tag != "" {
+				filterTags = append(filterTags, tag)
+			}
+		}
+	}
+	return filterTags
+}
+
+func getFilterOkFlag(request *http.Request) bool {
+	onlyProblemsStr := request.FormValue("onlyProblems")
+	if onlyProblemsStr != "" {
+		onlyProblems, _ := strconv.ParseBool(onlyProblemsStr)
+		return onlyProblems
+	}
+
+	filterOkCookie, err := request.Cookie("moira_filter_ok")
+	if err == http.ErrNoCookie {
+		return false
+	} else {
+		return filterOkCookie.Value == "true"
 	}
 }
