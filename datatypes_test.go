2b3839d8fa33b3b7bc0433e21260327957b866bc
Borovsky Arkady <borovskyav@list.ru>
2017-08-19 16:02:00 +0500

    datatypes tests

--- datatypes_test.go
+++ datatypes_test.go
@@ -59,15 +59,15 @@ func TestIsScheduleAllows(t *testing.T) {
 		schedule := getDefaultSchedule()
 		schedule.Days[0].Enabled = false
 		So(schedule.IsScheduleAllows(367980), ShouldBeFalse)
-		So(schedule.IsScheduleAllows(367980 + 86400), ShouldBeTrue)
-		So(schedule.IsScheduleAllows(367980 + 86400 * 2), ShouldBeTrue)
+		So(schedule.IsScheduleAllows(367980+86400), ShouldBeTrue)
+		So(schedule.IsScheduleAllows(367980+86400*2), ShouldBeTrue)
 	})
 
 	Convey("Exclude all days", t, func() {
 		schedule := allDaysExcludedSchedule
 		So(schedule.IsScheduleAllows(367980), ShouldBeFalse)
-		So(schedule.IsScheduleAllows(367980 + 86400), ShouldBeFalse)
-		So(schedule.IsScheduleAllows(367980 + 86400 * 5), ShouldBeFalse)
+		So(schedule.IsScheduleAllows(367980+86400), ShouldBeFalse)
+		So(schedule.IsScheduleAllows(367980+86400*5), ShouldBeFalse)
 	})
 
 	Convey("Include only morning", t, func() {
@@ -90,6 +90,96 @@ func TestIsScheduleAllows(t *testing.T) {
 		So(schedule.IsScheduleAllows(86400-242*60), ShouldBeTrue)  // 1/01/1970 19:58
 		So(schedule.IsScheduleAllows(86400+541*60), ShouldBeTrue)  // 2/01/1970 9:01
 		So(schedule.IsScheduleAllows(86400-255*60), ShouldBeTrue)  // 1/01/1970 19:45
+	})
+}
+
+func TestEventsData_GetSubjectState(t *testing.T) {
+	Convey("Get ERROR state", t, func() {
+		states := EventsData{{State: "OK"}, {State: "ERROR"}}
+		So(states.GetSubjectState(), ShouldResemble, "ERROR")
+	})
+}
+
+func TestTriggerData_GetTags(t *testing.T) {
+	Convey("Test one tag", t, func() {
+		triggerData := TriggerData{
+			Tags: []string{"tag1"},
+		}
+		So(triggerData.GetTags(), ShouldResemble, "[tag1]")
+	})
+	Convey("Test many tags", t, func() {
+		triggerData := TriggerData{
+			Tags: []string{"tag1", "tag2", "tag...orNot"},
+		}
+		So(triggerData.GetTags(), ShouldResemble, "[tag1][tag2][tag...orNot]")
+	})
+	Convey("Test no tags", t, func() {
+		triggerData := TriggerData{
+			Tags: make([]string, 0),
+		}
+		So(triggerData.GetTags(), ShouldBeEmpty)
+	})
+}
+
+func TestScheduledNotification_GetKey(t *testing.T) {
+	Convey("Get key", t, func() {
+		notification := ScheduledNotification{
+			Contact:   ContactData{Type: "email", Value: "my@mail.com"},
+			Event:     EventData{Value: nil, State: "NODATA", Metric: "my.metric"},
+			Timestamp: 123456789,
+		}
+		So(notification.GetKey(), ShouldResemble, "email:my@mail.com::my.metric:NODATA:0:0.000000:0:false:123456789")
+	})
+}
+
+func TestCheckData_GetOrCreateMetricState(t *testing.T) {
+	Convey("Test no metric", t, func() {
+		checkData := CheckData{
+			Metrics: make(map[string]MetricState),
+		}
+		So(checkData.GetOrCreateMetricState("my.metric", 12343), ShouldResemble, MetricState{State: "NODATA", Timestamp: 12343})
+	})
+	Convey("Test has metric", t, func() {
+		metricState := MetricState{Timestamp: 11211}
+		checkData := CheckData{
+			Metrics: map[string]MetricState{
+				"my.metric": metricState,
+			},
+		}
+		So(checkData.GetOrCreateMetricState("my.metric", 12343), ShouldResemble, metricState)
+	})
+}
+
+func TestMetricState_GetCheckPoint(t *testing.T) {
+	Convey("Get check point", t, func() {
+		metricState := MetricState{Timestamp: 800, EventTimestamp: 700}
+		So(metricState.GetCheckPoint(120), ShouldEqual, 700)
+
+		metricState = MetricState{Timestamp: 830, EventTimestamp: 700}
+		So(metricState.GetCheckPoint(120), ShouldEqual, 710)
+
+		metricState = MetricState{Timestamp: 699, EventTimestamp: 700}
+		So(metricState.GetCheckPoint(1), ShouldEqual, 700)
+	})
+}
+
+func TestMetricState_GetEventTimestamp(t *testing.T) {
+	Convey("Get event timestamp", t, func() {
+		metricState := MetricState{Timestamp: 800, EventTimestamp: 0}
+		So(metricState.GetEventTimestamp(), ShouldEqual, 800)
+
+		metricState = MetricState{Timestamp: 830, EventTimestamp: 700}
+		So(metricState.GetEventTimestamp(), ShouldEqual, 700)
+	})
+}
+
+func TestCheckData_GetEventTimestamp(t *testing.T) {
+	Convey("Get event timestamp", t, func() {
+		checkData := CheckData{Timestamp: 800, EventTimestamp: 0}
+		So(checkData.GetEventTimestamp(), ShouldEqual, 800)
+
+		checkData = CheckData{Timestamp: 830, EventTimestamp: 700}
+		So(checkData.GetEventTimestamp(), ShouldEqual, 700)
 	})
 }
 
