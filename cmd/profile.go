0cea50def25ef01be6055fc9f9a7a0bac7cfb7db
kamaev <hakariaruki@protonmail.com>
2017-12-01 01:26:59 +0500

    register handlers

--- cmd/profile.go
+++ cmd/profile.go
@@ -1,19 +1,28 @@
 package cmd
 
 import (
-	"github.com/moira-alert/moira"
 	"net/http"
-	_ "net/http/pprof"
+	"net/http/pprof"
+
+	"github.com/moira-alert/moira"
 )
 
 // StartProfiling starts http server with profiling data at given port
 func StartProfiling(logger moira.Logger, config ProfilerConfig) {
 
+	pprofMux := http.NewServeMux()
+	pprofMux.HandleFunc("/debug/pprof/", pprof.Index)
+	pprofMux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	pprofMux.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	pprofMux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	pprofMux.HandleFunc("/debug/pprof/trace", pprof.Trace)
+
 	go func() {
-		err := http.ListenAndServe(config.Port, nil)
+		err := http.ListenAndServe(config.Port, pprofMux)
 		if err != nil {
 			logger.Infof("Can't start pprof server: %v", err)
 			return
 		}
 	}()
+
 }
