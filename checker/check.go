c5b7fbe5bb6cf6c3c7eb1954918e80231912cc6e
Borovsky Arkady <borovskyav@list.ru>
2017-12-07 23:36:26 +0500

    Fix DEL state then no metrics again

--- checker/check.go
+++ checker/check.go
@@ -122,13 +122,10 @@ func (triggerChecker *TriggerChecker) handleErrorCheck(checkData moira.CheckData
 	if checkingError == ErrTriggerHasOnlyWildcards {
 		triggerChecker.Logger.Debugf("Trigger %s: %s", triggerChecker.TriggerID, checkingError.Error())
 		if len(checkData.Metrics) == 0 {
-			if triggerChecker.ttl == 0 {
+			if triggerChecker.ttl == 0 || triggerChecker.ttlState == DEL {
 				return checkData, nil
 			}
 			checkData.State = toMetricState(triggerChecker.ttlState)
-			if triggerChecker.ttlState == DEL {
-				return checkData, nil
-			}
 			if checkData.State != OK {
 				checkData.Message = "Trigger never received metrics"
 			}
