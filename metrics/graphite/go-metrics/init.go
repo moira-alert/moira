24c0dfd0af95612c71caec3a33bd301210830bf5
Oleg Matrokhin <matrokhin@gmail.com>
2018-01-15 14:01:57 +0300

    Hostname var in prefix (#59)
    * hostname variable in metrics prefix
    
    * use only part of hostname

--- metrics/graphite/go-metrics/init.go
+++ metrics/graphite/go-metrics/init.go
@@ -3,12 +3,16 @@ package metrics
 import (
 	"fmt"
 	"net"
+	"os"
+	"strings"
 
 	"github.com/rcrowley/go-metrics"
 
 	goMetricsGraphite "github.com/cyberdelia/go-metrics-graphite"
 	"github.com/moira-alert/moira/metrics/graphite"
 )
+
+const hostnameTmpl = "{hostname}"
 
 // Init is initializer for notifier graphite metrics worker based on go-metrics and go-metrics-graphite
 func Init(config graphite.Config) error {
@@ -17,8 +21,25 @@ func Init(config graphite.Config) error {
 		if err != nil {
 			return fmt.Errorf("Can not resolve graphiteURI %s: %s", config.URI, err)
 		}
-		go goMetricsGraphite.Graphite(metrics.DefaultRegistry, config.Interval, config.Prefix, address)
+		prefix, err := initPrefix(config.Prefix)
+		if err != nil {
+			return fmt.Errorf("Can not get OS hostname %s: %s", config.Prefix, err)
+		}
+
+		go goMetricsGraphite.Graphite(metrics.DefaultRegistry, config.Interval, prefix, address)
 		return nil
 	}
 	return nil
+}
+
+func initPrefix(prefix string) (string, error) {
+	if !strings.Contains(prefix, hostnameTmpl) {
+		return prefix, nil
+	}
+	hostname, err := os.Hostname()
+	if err != nil {
+		return prefix, err
+	}
+	short := strings.Split(hostname, ".")[0]
+	return strings.Replace(prefix, hostnameTmpl, short, -1), nil
 }
