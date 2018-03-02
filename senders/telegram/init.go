7a14384d4cff44fa504931ac91f297af468a9bea
Timur Kamaev <hakariaruki@protonmail.com>
2018-03-03 02:29:28 +0500

    Telegram bot graceful shutdown (#67)
    * refactorTelegramBot
    
    * refactorTelegramBot2
    
    * refactorTelegramBot3
    
    * refactorTelegramBot4

---
+++ senders/telegram/init.go
@@ -0,0 +1,104 @@
+package telegram
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/tucnak/telebot"
+
+	"github.com/moira-alert/moira"
+)
+
+const messenger = "telegram"
+
+var (
+	telegramMessageLimit    = 4096
+	pollerTimeout           = 10 * time.Second
+	databaseMutexExpiry     = 30 * time.Second
+	singlePollerStateExpiry = time.Minute
+	emojiStates             = map[string]string{
+		"OK":     "\xe2\x9c\x85",
+		"WARN":   "\xe2\x9a\xa0",
+		"ERROR":  "\xe2\xad\x95",
+		"NODATA": "\xf0\x9f\x92\xa3",
+		"TEST":   "\xf0\x9f\x98\x8a",
+	}
+)
+
+// Sender implements moira sender interface via telegram
+type Sender struct {
+	DataBase moira.Database
+	APIToken string
+	FrontURI string
+	logger   moira.Logger
+	bot      *telebot.Bot
+	location *time.Location
+}
+
+// Init loads yaml config, configures and starts telegram bot
+func (sender *Sender) Init(senderSettings map[string]string, logger moira.Logger, location *time.Location) error {
+	var err error
+	sender.APIToken = senderSettings["api_token"]
+	if sender.APIToken == "" {
+		return fmt.Errorf("can not read telegram api_token from config")
+	}
+	sender.FrontURI = senderSettings["front_uri"]
+	sender.logger = logger
+	sender.location = location
+
+	sender.bot, err = telebot.NewBot(telebot.Settings{
+		Token:  sender.APIToken,
+		Poller: &telebot.LongPoller{Timeout: pollerTimeout},
+	})
+	if err != nil {
+		return err
+	}
+
+	sender.bot.Handle(telebot.OnText, func(message *telebot.Message) {
+		if err = sender.handleMessage(message); err != nil {
+			sender.logger.Errorf("Error handling incoming message: %s", err.Error())
+		}
+	})
+
+	err = sender.runTelebot()
+	if err != nil {
+		return fmt.Errorf("error running bot: %s", err.Error())
+	}
+	return nil
+}
+
+// runTelebot starts telegram bot and manages bot subscriptions
+// to make sure there is always only one working Poller
+func (sender *Sender) runTelebot() error {
+	firstCheck := true
+	go func() {
+		for {
+			if sender.DataBase.RegisterBotIfAlreadyNot(messenger, databaseMutexExpiry) {
+				sender.logger.Infof("Registered new %s bot, checking for new messages", messenger)
+				go sender.bot.Start()
+				sender.renewSubscription(databaseMutexExpiry)
+				continue
+			}
+			if firstCheck {
+				sender.logger.Infof("%s bot already registered, trying for register every %v in loop", messenger, singlePollerStateExpiry)
+				firstCheck = false
+			}
+			<-time.After(singlePollerStateExpiry)
+		}
+	}()
+	return nil
+}
+
+// renewSubscription tries to renew bot subscription
+// and gracefully stops bot on fail to prevent multiple Poller instances running
+func (sender *Sender) renewSubscription(ttl time.Duration) {
+	checkTicker := time.NewTicker((ttl / time.Second) / 2 * time.Second)
+	for {
+		<-checkTicker.C
+		if !sender.DataBase.RenewBotRegistration(messenger) {
+			sender.logger.Warningf("Could not renew subscription for %s bot, try to register bot again", messenger)
+			sender.bot.Stop()
+			return
+		}
+	}
+}
---
+++ senders/telegram/send.go
@@ -0,0 +1,73 @@
+package telegram
+
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/moira-alert/moira"
+)
+
+// SendEvents implements Sender interface Send
+func (sender *Sender) SendEvents(events moira.NotificationEvents, contact moira.ContactData, trigger moira.TriggerData, throttled bool) error {
+	var message bytes.Buffer
+	state := events.GetSubjectState()
+	tags := trigger.GetTags()
+	emoji := emojiStates[state]
+
+	message.WriteString(fmt.Sprintf("%s%s %s %s (%d)\n", emoji, state, trigger.Name, tags, len(events)))
+
+	messageLimitReached := false
+	lineCount := 0
+
+	for _, event := range events {
+		value := strconv.FormatFloat(moira.UseFloat64(event.Value), 'f', -1, 64)
+		eventTime := time.Unix(event.Timestamp, 0).In(sender.location)
+		line := fmt.Sprintf("\n%s: %s = %s (%s to %s)", eventTime.Format("15:04"), event.Metric, value, event.OldState, event.State)
+		if len(moira.UseString(event.Message)) > 0 {
+			line += fmt.Sprintf(". %s", moira.UseString(event.Message))
+		}
+		if message.Len()+len(line) > telegramMessageLimit-400 {
+			messageLimitReached = true
+			break
+		}
+		message.WriteString(line)
+		lineCount++
+	}
+
+	if messageLimitReached {
+		message.WriteString(fmt.Sprintf("\n\n...and %d more events.", len(events)-lineCount))
+	}
+
+	message.WriteString(fmt.Sprintf("\n\n%s/trigger/%s\n", sender.FrontURI, events[0].TriggerID))
+
+	if throttled {
+		message.WriteString("\nPlease, fix your system or tune this trigger to generate less events.")
+	}
+
+	sender.logger.Debugf("Calling telegram api with chat_id %s and message body %s", contact.Value, message.String())
+
+	if err := sender.talk(contact.Value, message.String()); err != nil {
+		return fmt.Errorf("Failed to send message to telegram contact %s: %s. ", contact.Value, err)
+	}
+	return nil
+}
+
+// talk processes one talk
+func (sender *Sender) talk(username, message string) error {
+	var err error
+	uid, err := sender.DataBase.GetIDByUsername(messenger, username)
+	if err != nil {
+		return fmt.Errorf("failed to get username uuid: %s", err.Error())
+	}
+	chat, err := sender.bot.ChatByID(uid)
+	if err != nil {
+		return fmt.Errorf("can't find recepient %s: %s", uid, err.Error())
+	}
+	_, err = sender.bot.Send(chat, message)
+	if err != nil {
+		return fmt.Errorf("can't send message [%s] to %s: %s", message, uid, err.Error())
+	}
+	return nil
+}
