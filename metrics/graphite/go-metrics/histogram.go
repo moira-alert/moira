d16c558958de97258f7ad4ce4de4a7d99b4d8fcb
Borovsky Arkady <borovskyav@list.ru>
2018-03-08 20:04:47 +0500

    Fix histogram pointer argument

--- metrics/graphite/go-metrics/histogram.go
+++ metrics/graphite/go-metrics/histogram.go
@@ -16,46 +16,46 @@ func registerHistogram(name string) *ExpDecayHistogram {
 	return &ExpDecayHistogram{goMetrics.NewRegisteredHistogram(name, goMetrics.DefaultRegistry, goMetrics.NewExpDecaySample(1028, 0.015))}
 }
 
-func (histogram ExpDecayHistogram) Clear() {
+func (histogram *ExpDecayHistogram) Clear() {
 	histogram.histogram.Clear()
 }
 
-func (histogram ExpDecayHistogram) Count() int64 {
+func (histogram *ExpDecayHistogram) Count() int64 {
 	return histogram.histogram.Count()
 }
 
-func (histogram ExpDecayHistogram) Max() int64 {
+func (histogram *ExpDecayHistogram) Max() int64 {
 	return histogram.histogram.Max()
 }
 
-func (histogram ExpDecayHistogram) Mean() float64 {
+func (histogram *ExpDecayHistogram) Mean() float64 {
 	return histogram.histogram.Mean()
 }
 
-func (histogram ExpDecayHistogram) Min() int64 {
+func (histogram *ExpDecayHistogram) Min() int64 {
 	return histogram.histogram.Min()
 }
 
-func (histogram ExpDecayHistogram) Percentile(p float64) float64 {
+func (histogram *ExpDecayHistogram) Percentile(p float64) float64 {
 	return histogram.histogram.Percentile(p)
 }
 
-func (histogram ExpDecayHistogram) Percentiles(p []float64) []float64 {
+func (histogram *ExpDecayHistogram) Percentiles(p []float64) []float64 {
 	return histogram.histogram.Percentiles(p)
 }
 
-func (histogram ExpDecayHistogram) StdDev() float64 {
+func (histogram *ExpDecayHistogram) StdDev() float64 {
 	return histogram.histogram.StdDev()
 }
 
-func (histogram ExpDecayHistogram) Sum() int64 {
+func (histogram *ExpDecayHistogram) Sum() int64 {
 	return histogram.histogram.Sum()
 }
 
-func (histogram ExpDecayHistogram) Update(v int64) {
+func (histogram *ExpDecayHistogram) Update(v int64) {
 	histogram.histogram.Update(v)
 }
 
-func (histogram ExpDecayHistogram) Variance() float64 {
+func (histogram *ExpDecayHistogram) Variance() float64 {
 	return histogram.histogram.Variance()
 }
