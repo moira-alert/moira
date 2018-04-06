6804809889bec9b39ceb057d67f626f721f87f9e
kamaev <hakariaruki@protonmail.com>
2018-04-06 17:34:21 +0500

    Save matched metrics in parralell

--- filter/matched_metrics/metrics.go
+++ filter/matched_metrics/metrics.go
@@ -7,6 +7,7 @@ import (
 	"github.com/moira-alert/moira"
 	"github.com/moira-alert/moira/filter"
 	"github.com/moira-alert/moira/metrics/graphite"
+	"runtime"
 )
 
 // MetricsMatcher make buffer of metrics and save it
@@ -31,33 +32,38 @@ func NewMetricsMatcher(metrics *graphite.FilterMetrics, logger moira.Logger, dat
 
 // Start process matched metrics from channel and save it in cache storage
 func (matcher *MetricsMatcher) Start(channel chan *moira.MatchedMetric) {
-	matcher.waitGroup.Add(1)
-	go func() {
-		defer matcher.waitGroup.Done()
-		buffer := make(map[string]*moira.MatchedMetric)
-		for {
-			select {
-			case metric, ok := <-channel:
-				if !ok {
-					matcher.logger.Info("Moira Filter Metrics Matcher stopped")
-					return
-				}
-				matcher.cacheStorage.EnrichMatchedMetric(buffer, metric)
-				if len(buffer) < 1000 {
-					continue
-				}
-			case <-time.After(time.Second):
+	numCPU := runtime.NumCPU()
+	matcher.waitGroup.Add(numCPU)
+	for i := 0; i < numCPU; i++ {
+		go matcher.startSaver(channel)
+	}
+	matcher.logger.Info("Moira Filter Metrics Matcher started")
+}
+
+func (matcher *MetricsMatcher) startSaver(channel chan *moira.MatchedMetric) {
+	defer matcher.waitGroup.Done()
+	buffer := make(map[string]*moira.MatchedMetric)
+	for {
+		select {
+		case metric, ok := <-channel:
+			if !ok {
+				matcher.logger.Info("Moira Filter Metrics Matcher stopped")
+				return
 			}
-			if len(buffer) == 0 {
+			matcher.cacheStorage.EnrichMatchedMetric(buffer, metric)
+			if len(buffer) < 1000 {
 				continue
 			}
-			timer := time.Now()
-			matcher.save(buffer)
-			matcher.metrics.SavingTimer.UpdateSince(timer)
-			buffer = make(map[string]*moira.MatchedMetric)
+		case <-time.After(time.Second):
 		}
-	}()
-	matcher.logger.Info("Moira Filter Metrics Matcher started")
+		if len(buffer) == 0 {
+			continue
+		}
+		timer := time.Now()
+		matcher.save(buffer)
+		matcher.metrics.SavingTimer.UpdateSince(timer)
+		buffer = make(map[string]*moira.MatchedMetric)
+	}
 }
 
 // Wait waits for metric matcher instance will stop
