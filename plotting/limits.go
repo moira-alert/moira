a804d83cedb25a8a077a2ce5083b84a29d00cec9
kamaev <hakariaruki@protonmail.com>
2018-05-25 19:28:50 +0500

    fix limits

--- plotting/limits.go
+++ plotting/limits.go
@@ -5,7 +5,7 @@ import (
 	"math"
 
 	"github.com/go-graphite/carbonapi/expr/types"
-	//"github.com/wcharczuk/go-chart/util"
+	"github.com/wcharczuk/go-chart/util"
 )
 
 // Limits is a set of limits for given metricsData
@@ -18,23 +18,15 @@ type Limits struct {
 
 // ResolveLimits returns common plot limits
 func ResolveLimits(metricsData []*types.MetricData, from int32, to int32) Limits {
-	var lowest float64
-	var highest float64
+	allValues := make([]float64, 0)
 	for _, metricData := range metricsData {
-		startInd, _ := ResolveFirstPoint(metricData)
-		for metricValInd := startInd; metricValInd < len(metricData.Values); metricValInd++ {
-			metricVal := metricData.Values[metricValInd]
-			if metricValInd == startInd {
-				lowest = metricVal
-				highest = metricVal
-			}
-			if !math.IsNaN(metricVal) {
-				lowest = math.Min(lowest, metricVal)
-				highest = math.Max(highest, metricVal)
-				//lowest, highest = util.Math.MinAndMax(lowest, highest, metricVal)
+		for _, metricValue := range metricData.Values {
+			if !math.IsNaN(metricValue) {
+				allValues = append(allValues, metricValue)
 			}
 		}
 	}
+	lowest, highest := util.Math.MinAndMax(allValues...)
 	return Limits{
 		From:    Int32ToTime(from),
 		To:      Int32ToTime(to),
