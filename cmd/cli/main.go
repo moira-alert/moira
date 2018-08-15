4da643341edcb8032c3fc1a44515bcbbebfe46dd
kamaev <hakariaruki@protonmail.com>
2018-08-15 13:08:29 +0500

    add logger to converters

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -33,9 +33,9 @@ var (
 )
 
 const (
-	warningsTag              = "ERROR"
-	recoveringsTag           = "DEGRADATION"
-	deprecatedRecoveringsTag = "HIGH DEGRADATION"
+	stateErrorTag           = "ERROR"
+	stateDegradationTag     = "DEGRADATION"
+	stateHighDegradationTag = "HIGH DEGRADATION"
 )
 
 func main() {
@@ -60,14 +60,14 @@ func main() {
 		os.Exit(1)
 	}
 
-	log, err := logging.ConfigureLog(config.LogFile, config.LogLevel, "cli")
+	logger, err := logging.ConfigureLog(config.LogFile, config.LogLevel, "cli")
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Can't configure main logger: %v\n", err)
 		os.Exit(1)
 	}
 
 	databaseSettings := config.Redis.GetSettings()
-	dataBase := redis.NewDatabase(log, databaseSettings)
+	dataBase := redis.NewDatabase(logger, databaseSettings)
 
 	if *convertPythonExpressions {
 		if err := ConvertPythonExpressions(dataBase); err != nil {
@@ -98,33 +98,33 @@ func main() {
 	}
 
 	if *updateDatabaseStructures {
-		fmt.Println("Start updating existing trigger structures into new format")
-		if err := ConvertTriggers(dataBase, false); err != nil {
-			fmt.Printf("Can not update existing triggers: %s\n", err.Error())
+		logger.Info("Start updating existing trigger structures into new format")
+		if err := ConvertTriggers(dataBase, logger, false); err != nil {
+			logger.Errorf("Can not update existing triggers: %s", err.Error())
 		} else {
-			fmt.Println("Trigger structures has been sucessfully updated")
+			logger.Info("Trigger structures has been sucessfully updated")
 		}
-		fmt.Println("Start updating existing subscription structures into new format")
-		if err := ConvertSubscriptions(dataBase, false); err != nil {
-			fmt.Printf("Can not update existing subscriptions: %s\n", err.Error())
+		logger.Info("Start updating existing subscription structures into new format")
+		if err := ConvertSubscriptions(dataBase, logger, false); err != nil {
+			logger.Errorf("Can not update existing subscriptions: %s", err.Error())
 		} else {
-			fmt.Println("Subscription structures has been sucessfully updated")
+			logger.Info("Subscription structures has been sucessfully updated")
 		}
 	}
 
 	if *downgradeDatabaseStructures {
 		// ToDo: In future: ask which version of Moira structures use to downgrade
-		fmt.Println("Start downgrading existing trigger structures into old format")
-		if err := ConvertTriggers(dataBase, true); err != nil {
-			fmt.Printf("Can not downgrade existing triggers: %s\n", err.Error())
+		logger.Info("Start downgrading existing trigger structures into old format")
+		if err := ConvertTriggers(dataBase, logger, true); err != nil {
+			logger.Errorf("Can not downgrade existing triggers: %s", err.Error())
 		} else {
-			fmt.Println("Trigger structures has been sucessfully downgraded")
+			logger.Info("Trigger structures has been sucessfully downgraded")
 		}
-		fmt.Println("Start downgrading existing subscription structures into old format")
-		if err := ConvertSubscriptions(dataBase, true); err != nil {
-			fmt.Printf("Can not downgrade existing subscriptions: %s\n", err.Error())
+		logger.Info("Start downgrading existing subscription structures into old format")
+		if err := ConvertSubscriptions(dataBase, logger, true); err != nil {
+			logger.Errorf("Can not downgrade existing subscriptions: %s", err.Error())
 		} else {
-			fmt.Println("Subscription structures has been sucessfully downgraded")
+			logger.Info("Subscription structures has been sucessfully downgraded")
 		}
 	}
 }
@@ -256,15 +256,15 @@ func ConvertPythonExpressions(dataBase moira.Database) error {
 
 // ConvertTaggedSubscription checks that subscription has deprecated pseudo-tags
 // and adds corresponding fields into subscription to store actual json structure in redis
-func ConvertTaggedSubscription(database moira.Database, subscription *moira.SubscriptionData) {
+func ConvertTaggedSubscription(database moira.Database, subscription *moira.SubscriptionData) error {
 	newTags := make([]string, 0)
 	for _, tag := range subscription.Tags {
 		switch tag {
-		case warningsTag:
+		case stateErrorTag:
 			if !subscription.IgnoreWarnings {
 				subscription.IgnoreWarnings = true
 			}
-		case recoveringsTag, deprecatedRecoveringsTag:
+		case stateDegradationTag, stateHighDegradationTag:
 			if !subscription.IgnoreRecoverings {
 				subscription.IgnoreRecoverings = true
 			}
@@ -273,25 +273,25 @@ func ConvertTaggedSubscription(database moira.Database, subscription *moira.Subs
 		}
 	}
 	subscription.Tags = newTags
-	database.SaveSubscription(subscription)
+	return database.SaveSubscription(subscription)
 }
 
 // ConvertUntaggedSubscription can be used in rollback if something will go wrong after Moira update.
 // This method checks that subscription must ignore specific states transitions and adds required pseudo-tags to existing subscription's tags.
-func ConvertUntaggedSubscription(database moira.Database, subscription *moira.SubscriptionData) {
-	if subscription.IgnoreWarnings && !subscriptionHasTag(subscription, warningsTag) {
-		subscription.Tags = append(subscription.Tags, warningsTag)
+func ConvertUntaggedSubscription(database moira.Database, subscription *moira.SubscriptionData) error {
+	if subscription.IgnoreWarnings && !subscriptionHasTag(subscription, stateErrorTag) {
+		subscription.Tags = append(subscription.Tags, stateErrorTag)
 	}
-	if subscription.IgnoreRecoverings && !subscriptionHasTag(subscription, recoveringsTag) {
-		subscription.Tags = append(subscription.Tags, recoveringsTag)
+	if subscription.IgnoreRecoverings && !subscriptionHasTag(subscription, stateDegradationTag) {
+		subscription.Tags = append(subscription.Tags, stateDegradationTag)
 	}
-	database.SaveSubscription(subscription)
+	return database.SaveSubscription(subscription)
 }
 
 // ConvertSubscriptions converts all existing tag subscriptions under specified convertation strategy
 // In versions older than 2.3 Moira used to check if subscription has special pseudo-tags to ignore trigger's states transitions such as "WARN<->OK" or "ERROR->OK"
 // which can be specified in subscription parameters. Starting from version 2.3 this tags are deprecated and Moira uses corresponding boolean fields instead.
-func ConvertSubscriptions(database moira.Database, rollback bool) error {
+func ConvertSubscriptions(database moira.Database, logger moira.Logger, rollback bool) error {
 	allTags, err := database.GetTagNames()
 	if err != nil {
 		return err
@@ -300,7 +300,7 @@ func ConvertSubscriptions(database moira.Database, rollback bool) error {
 	if err != nil {
 		return err
 	}
-	var subscriptionsConverter func(moira.Database, *moira.SubscriptionData)
+	var subscriptionsConverter func(moira.Database, *moira.SubscriptionData) error
 	if !rollback {
 		subscriptionsConverter = ConvertTaggedSubscription
 	} else {
@@ -308,10 +308,14 @@ func ConvertSubscriptions(database moira.Database, rollback bool) error {
 	}
 	for _, subscription := range allSubscriptions {
 		if subscription != nil {
-			subscriptionsConverter(database, subscription)
-			convertedMessage := fmt.Sprintf("Subscription %s has been succesfully converted. Tags: %s IgnoreWarnings: %t IgnoreRecoverings: %t",
-				subscription.ID, strings.Join(subscription.Tags, ", "), subscription.IgnoreWarnings, subscription.IgnoreRecoverings)
-			fmt.Println(convertedMessage)
+			if err := subscriptionsConverter(database, subscription); err != nil {
+				convertedMessage := fmt.Sprintf("Subscription %s has been succesfully converted. Tags: %s IgnoreWarnings: %t IgnoreRecoverings: %t",
+					subscription.ID, strings.Join(subscription.Tags, ", "), subscription.IgnoreWarnings, subscription.IgnoreRecoverings)
+				logger.Info(convertedMessage)
+			} else {
+				convertedMessage := fmt.Sprintf("An error occurred due to convertation procees of subscription %s: %s", subscription.ID, err.Error())
+				logger.Error(convertedMessage)
+			}
 		}
 	}
 	return nil
@@ -320,7 +324,7 @@ func ConvertSubscriptions(database moira.Database, rollback bool) error {
 // ConvertTriggers converts all existing triggers  in following strategy:
 // - update: Set trigger_type to one of the following options: "expression" (trigger has custom user expression) "rising" (error > warn > ok), "falling" (error < warn < ok)
 // - rollback: Set trigger_type to empty string and fill omitted warn/error values
-func ConvertTriggers(dataBase moira.Database, rollback bool) error {
+func ConvertTriggers(dataBase moira.Database, logger moira.Logger, rollback bool) error {
 	allTriggerIDs, err := dataBase.GetTriggerIDs()
 	if err != nil {
 		return err
@@ -332,10 +336,10 @@ func ConvertTriggers(dataBase moira.Database, rollback bool) error {
 	}
 
 	if rollback {
-		return downgradeTriggers(allTriggers, dataBase)
+		return downgradeTriggers(allTriggers, dataBase, logger)
 	}
 
-	return updateTriggers(allTriggers, dataBase)
+	return updateTriggers(allTriggers, dataBase, logger)
 }
 
 func subscriptionHasTag(subscription *moira.SubscriptionData, tag string) bool {
@@ -347,7 +351,7 @@ func subscriptionHasTag(subscription *moira.SubscriptionData, tag string) bool {
 	return false
 }
 
-func updateTriggers(triggers []*moira.Trigger, dataBase moira.Database) error {
+func updateTriggers(triggers []*moira.Trigger, dataBase moira.Database, logger moira.Logger) error {
 	for _, trigger := range triggers {
 		if trigger == nil {
 			continue
@@ -355,12 +359,12 @@ func updateTriggers(triggers []*moira.Trigger, dataBase moira.Database) error {
 		if trigger.TriggerType == moira.RisingTrigger ||
 			trigger.TriggerType == moira.FallingTrigger ||
 			trigger.TriggerType == moira.ExpressionTrigger {
-			fmt.Printf("Trigger %v has '%v' type - no need to convert\n", trigger.ID, trigger.TriggerType)
+			logger.Infof("Trigger %v has '%v' type - no need to convert", trigger.ID, trigger.TriggerType)
 			continue
 		}
 
-		if err := setProperTriggerType(trigger); err == nil {
-			fmt.Printf("Trigger %v - save to Database\n", trigger.ID)
+		if err := setProperTriggerType(trigger, logger); err == nil {
+			logger.Infof("Trigger %v - save to Database", trigger.ID)
 			if err := dataBase.SaveTrigger(trigger.ID, trigger); err != nil {
 				return err
 			}
@@ -372,14 +376,14 @@ func updateTriggers(triggers []*moira.Trigger, dataBase moira.Database) error {
 	return nil
 }
 
-func downgradeTriggers(triggers []*moira.Trigger, dataBase moira.Database) error {
+func downgradeTriggers(triggers []*moira.Trigger, dataBase moira.Database, logger moira.Logger) error {
 	for _, trigger := range triggers {
 		if trigger == nil {
 			continue
 		}
 
-		if err := setProperWarnErrorExpressionValues(trigger); err == nil {
-			fmt.Printf("Trigger %v - save to Database\n", trigger.ID)
+		if err := setProperWarnErrorExpressionValues(trigger, logger); err == nil {
+			logger.Infof("Trigger %v - save to Database", trigger.ID)
 			if err := dataBase.SaveTrigger(trigger.ID, trigger); err != nil {
 				return err
 			}
@@ -391,30 +395,30 @@ func downgradeTriggers(triggers []*moira.Trigger, dataBase moira.Database) error
 	return nil
 }
 
-func setProperTriggerType(trigger *moira.Trigger) error {
-	fmt.Printf("Trigger %v, trigger_type: '%v' - start conversion\n", trigger.ID, trigger.TriggerType)
+func setProperTriggerType(trigger *moira.Trigger, logger moira.Logger) error {
+	logger.Infof("Trigger %v, trigger_type: '%v' - start conversion", trigger.ID, trigger.TriggerType)
 	if trigger.Expression != nil && *trigger.Expression != "" {
-		fmt.Printf("Trigger %v has expression '%v' - set trigger_type to '%v'...\n",
+		logger.Infof("Trigger %v has expression '%v' - set trigger_type to '%v'...",
 			trigger.ID, *trigger.Expression, moira.ExpressionTrigger)
 		trigger.TriggerType = moira.ExpressionTrigger
 		return nil
 	}
 
 	if trigger.WarnValue != nil && trigger.ErrorValue != nil {
-		fmt.Printf("Trigger %v - warn_value: %v, error_value: %v\n",
+		logger.Infof("Trigger %v - warn_value: %v, error_value: %v",
 			trigger.ID, trigger.WarnValue, trigger.ErrorValue)
 		if *trigger.ErrorValue > *trigger.WarnValue {
-			fmt.Printf("Trigger %v - set trigger_type to '%v'\n", trigger.ID, moira.RisingTrigger)
+			logger.Infof("Trigger %v - set trigger_type to '%v'", trigger.ID, moira.RisingTrigger)
 			trigger.TriggerType = moira.RisingTrigger
 			return nil
 		}
 		if *trigger.ErrorValue < *trigger.WarnValue {
-			fmt.Printf("Trigger %v - set trigger_type to '%v'\n", trigger.ID, moira.FallingTrigger)
+			logger.Infof("Trigger %v - set trigger_type to '%v'", trigger.ID, moira.FallingTrigger)
 			trigger.TriggerType = moira.FallingTrigger
 			return nil
 		}
 		if *trigger.ErrorValue == *trigger.WarnValue {
-			fmt.Printf("Trigger %v - warn_value == error_value, set trigger_type to '%v', set warn_value to 'nil'\n",
+			logger.Infof("Trigger %v - warn_value == error_value, set trigger_type to '%v', set warn_value to 'nil'",
 				trigger.ID, moira.RisingTrigger)
 			trigger.TriggerType = moira.RisingTrigger
 			trigger.WarnValue = nil
@@ -425,34 +429,34 @@ func setProperTriggerType(trigger *moira.Trigger) error {
 		trigger.ID, trigger.WarnValue, trigger.ErrorValue, trigger.Expression)
 }
 
-func setProperWarnErrorExpressionValues(trigger *moira.Trigger) error {
+func setProperWarnErrorExpressionValues(trigger *moira.Trigger, logger moira.Logger) error {
 	expr := ""
 	if trigger.Expression != nil {
 		expr = *trigger.Expression
 	}
-	fmt.Printf("Trigger %v: warn_value: %d, error_value: %d, expression: '%s', trigger_type: '%v', - start conversion\n",
+	logger.Infof("Trigger %v: warn_value: %d, error_value: %d, expression: '%s', trigger_type: '%v', - start conversion",
 		trigger.ID, trigger.WarnValue, trigger.ErrorValue, expr, trigger.TriggerType)
 	if trigger.TriggerType == moira.ExpressionTrigger &&
 		expr != "" {
-		fmt.Printf("Trigger %v has expression '%s' - set trigger_type to ''\n", trigger.ID, expr)
+		logger.Infof("Trigger %v has expression '%s' - set trigger_type to ''", trigger.ID, expr)
 		trigger.TriggerType = ""
 		return nil
 	}
 	if trigger.WarnValue != nil && trigger.ErrorValue != nil {
-		fmt.Printf("Trigger %v has warn_value '%f', error_value '%v' - set trigger_type to ''\n",
+		logger.Infof("Trigger %v has warn_value '%f', error_value '%v' - set trigger_type to ''",
 			trigger.ID, *trigger.WarnValue, *trigger.ErrorValue)
 		trigger.TriggerType = ""
 		return nil
 	}
 	if trigger.WarnValue == nil && trigger.ErrorValue != nil {
-		fmt.Printf("Trigger %v has warn_value '<nil>', error_value '%v' - set trigger_type to '' and update warn_value to '%v'\n",
+		logger.Infof("Trigger %v has warn_value '<nil>', error_value '%v' - set trigger_type to '' and update warn_value to '%v'",
 			trigger.ID, *trigger.ErrorValue, *trigger.ErrorValue)
 		trigger.WarnValue = trigger.ErrorValue
 		trigger.TriggerType = ""
 		return nil
 	}
 	if trigger.WarnValue != nil && trigger.ErrorValue == nil {
-		fmt.Printf("Trigger %v has warn_value '%v', error_value '<nil>' - set trigger_type to '' and update error_value to '%v'\n",
+		logger.Infof("Trigger %v has warn_value '%v', error_value '<nil>' - set trigger_type to '' and update error_value to '%v'",
 			trigger.ID, *trigger.WarnValue, *trigger.WarnValue)
 		trigger.ErrorValue = trigger.WarnValue
 		trigger.TriggerType = ""
