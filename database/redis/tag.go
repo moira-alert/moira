035baadc9239fd7c804c77960fa9b74cca34b7b2
Denis Titusov <sescusu@gmail.com>
2018-10-24 15:24:41 +0500

    fix linter

--- database/redis/tag.go
+++ database/redis/tag.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 
 	"github.com/garyburd/redigo/redis"
-	"github.com/moira-alert/moira/database"
 )
 
 // GetTagNames returns all tags from set with tag data
@@ -46,42 +45,6 @@ func (connector *DbConnector) GetTagTriggerIDs(tagName string) ([]string, error)
 			return make([]string, 0), nil
 		}
 		return nil, fmt.Errorf("Failed to retrieve tag triggers:%s, err: %s", tagName, err.Error())
-	}
-	return triggerIDs, nil
-}
-
-func (connector *DbConnector) getTagsTriggerIDs(tagNames []string) ([]string, error) {
-	c := connector.pool.Get()
-	defer c.Close()
-
-	c.Send("MULTI")
-	for _, tagName := range tagNames {
-		c.Send("SMEMBERS", tagTriggersKey(tagName))
-	}
-
-	rawResponse, err := redis.Values(c.Do("EXEC"))
-	if err != nil {
-		return nil, fmt.Errorf("failed to EXEC: %s", err.Error())
-	}
-
-	triggerIDsByTags := make(map[string]bool, 0)
-
-	for _, triggersArray := range rawResponse {
-		tagTriggerIDs, err := redis.Strings(triggersArray, nil)
-		if err != nil {
-			if err == database.ErrNil {
-				continue
-			}
-			return nil, fmt.Errorf("failed to retrieve tags triggers: %s", err.Error())
-		}
-		for _, triggerID := range tagTriggerIDs {
-			triggerIDsByTags[triggerID] = true
-		}
-	}
-
-	triggerIDs := make([]string, 0)
-	for triggerID := range triggerIDsByTags {
-		triggerIDs = append(triggerIDs, triggerID)
 	}
 	return triggerIDs, nil
 }
