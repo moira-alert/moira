1ecc93157accd0951d2d2cc2de08e31518665131
Denis Titusov <sescusu@gmail.com>
2018-11-16 17:28:14 +0500

    Add benchmarks

--- index/index_test.go
+++ index/index_test.go
@@ -1,6 +1,7 @@
 package index
 
 import (
+	"math/rand"
 	"testing"
 
 	"github.com/blevesearch/bleve"
@@ -8,6 +9,7 @@ import (
 	"github.com/moira-alert/moira"
 	"github.com/moira-alert/moira/mock/moira-alert"
 	"github.com/op/go-logging"
+	"github.com/satori/go.uuid"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
@@ -111,12 +113,81 @@ func TestIndex_CreateAndFill(t *testing.T) {
 		So(err, ShouldNotBeNil)
 		So(docCount, ShouldEqual, 0)
 	})
+}
+
+func BenchmarkIndex_CreateAndFill(b *testing.B) {
+	mockCtrl := gomock.NewController(b)
+	database := mock_moira_alert.NewMockDatabase(mockCtrl)
+	logger, _ := logging.GetLogger("Benchmark")
+	defer mockCtrl.Finish()
+
+	var numberOfTriggers = 4000
+
+	triggerIDs := make([]string, numberOfTriggers)
+	for i := range triggerIDs {
+		triggerIDs[i] = uuid.NewV4().String()
+	}
+
+	triggersPointers := make([]*moira.TriggerCheck, numberOfTriggers)
+	for i := range triggerIDs {
+		triggersPointers[i] = &moira.TriggerCheck{
+			Trigger: moira.Trigger{
+				ID:   triggerIDs[i],
+				Name: randStringBytes(100),
+				Tags: []string{randStringBytes(5), randStringBytes(3)},
+			},
+			LastCheck: moira.CheckData{
+				Score: rand.Int63n(1000),
+			},
+		}
+	}
+
+	b.ResetTimer()
+	for batchSize := 1024; batchSize >= 128; batchSize = batchSize / 2 {
+		index := NewSearchIndex(logger, database)
+		index.createIndex()
+
+		chunkedTriggerIDs := moira.ChunkSlice(triggerIDs, batchSize)
+		chunkedTriggerPointers := chunkTriggerChecks(triggersPointers, batchSize)
+
+		for i, slice := range chunkedTriggerIDs {
+			database.EXPECT().GetTriggerChecks(slice).Return(chunkedTriggerPointers[i], nil)
+		}
+
+		index.addTriggers(triggerIDs, batchSize)
+	}
 
 }
 
 func (index *Index) destroyIndex() {
 	index.index.Close()
 }
+
+func randStringBytes(n int) string {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = charBytes[rand.Intn(len(charBytes))]
+	}
+	return string(b)
+}
+
+func chunkTriggerChecks(original []*moira.TriggerCheck, chunkSize int) (divided [][]*moira.TriggerCheck) {
+	if chunkSize < 1 {
+		return
+	}
+	for i := 0; i < len(original); i += chunkSize {
+		end := i + chunkSize
+
+		if end > len(original) {
+			end = len(original)
+		}
+
+		divided = append(divided, original[i:end])
+	}
+	return
+}
+
+const charBytes = ".,!?-_()+1234567890 abcdefghijklmnopqrstuvwxyz"
 
 var triggerChecks = []moira.TriggerCheck{
 	{
