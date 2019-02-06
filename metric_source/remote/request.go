16b9de920a09af3e9519736415e473baee2d06de
Timur Kamaev <hakariaruki@protonmail.com>
2019-02-06 13:57:43 +0500

    [remote triggers] always close response body (#237)
    * always close response body
    
    * set correct return tuple

--- metric_source/remote/request.go
+++ metric_source/remote/request.go
@@ -25,20 +25,20 @@ func (remote *Remote) prepareRequest(from, until int64, target string) (*http.Re
 }
 
 func (remote *Remote) makeRequest(req *http.Request) ([]byte, error) {
+	var body []byte
 	resp, err := remote.client.Do(req)
-	if err != nil {
-		return nil, err
+	if resp != nil {
+		defer resp.Body.Close()
 	}
-	body, err := ioutil.ReadAll(resp.Body)
-	defer resp.Body.Close()
-
 	if err != nil {
 		return body, err
 	}
-
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return body, err
+	}
 	if resp.StatusCode != 200 {
-		err = fmt.Errorf("bad response status %d: %s", resp.StatusCode, string(body))
-		return body, err
+		return body, fmt.Errorf("bad response status %d: %s", resp.StatusCode, string(body))
 	}
-	return body, err
+	return body, nil
 }
