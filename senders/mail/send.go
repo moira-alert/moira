7dca64f46b557b0bca3be2bc9519789eedd9dcdb
Borovsky Arkady <borovskyav@list.ru>
2019-02-07 21:34:33 +0500

    Mail small refactor and tests

---
+++ senders/mail/send.go
@@ -0,0 +1,118 @@
+package mail
+
+import (
+	"crypto/tls"
+	"fmt"
+	"html/template"
+	"io"
+	"net/smtp"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/moira-alert/moira"
+	"gopkg.in/gomail.v2"
+)
+
+type templateRow struct {
+	Metric     string
+	Timestamp  string
+	Oldstate   string
+	State      string
+	Value      string
+	WarnValue  string
+	ErrorValue string
+	Message    string
+}
+
+type triggerData struct {
+	Link         string
+	Description  template.HTML
+	Throttled    bool
+	TriggerName  string
+	Tags         string
+	TriggerState string
+	Items        []*templateRow
+	PlotCID      string
+}
+
+// SendEvents implements Sender interface Send
+func (sender *Sender) SendEvents(events moira.NotificationEvents, contact moira.ContactData, trigger moira.TriggerData, plot []byte, throttled bool) error {
+	message := sender.makeMessage(events, contact, trigger, plot, throttled)
+	return sender.dialAndSend(message)
+}
+
+func (sender *Sender) makeMessage(events moira.NotificationEvents, contact moira.ContactData, trigger moira.TriggerData, plot []byte, throttled bool) *gomail.Message {
+	state := events.GetSubjectState()
+	tags := trigger.GetTags()
+
+	subject := fmt.Sprintf("%s %s %s (%d)", state, trigger.Name, tags, len(events))
+
+	templateData := triggerData{
+		Link:         fmt.Sprintf("%s/trigger/%s", sender.FrontURI, events[0].TriggerID),
+		Description:  formatDescription(trigger.Desc),
+		Throttled:    throttled,
+		TriggerName:  trigger.Name,
+		Tags:         tags,
+		TriggerState: state,
+		Items:        make([]*templateRow, 0, len(events)),
+	}
+
+	for _, event := range events {
+		templateData.Items = append(templateData.Items, &templateRow{
+			Metric:     event.Metric,
+			Timestamp:  time.Unix(event.Timestamp, 0).In(sender.location).Format(sender.dateTimeFormat),
+			Oldstate:   event.OldState,
+			State:      event.State,
+			Value:      strconv.FormatFloat(moira.UseFloat64(event.Value), 'f', -1, 64),
+			WarnValue:  strconv.FormatFloat(trigger.WarnValue, 'f', -1, 64),
+			ErrorValue: strconv.FormatFloat(trigger.ErrorValue, 'f', -1, 64),
+			Message:    moira.UseString(event.Message),
+		})
+	}
+
+	m := gomail.NewMessage()
+	m.SetHeader("From", sender.From)
+	m.SetHeader("To", contact.Value)
+	m.SetHeader("Subject", subject)
+
+	if len(plot) > 0 {
+		plotCID := "plot.png"
+		templateData.PlotCID = plotCID
+		m.Embed(plotCID, gomail.SetCopyFunc(func(w io.Writer) error {
+			_, err := w.Write(plot)
+			return err
+		}))
+	}
+
+	m.AddAlternativeWriter("text/html", func(w io.Writer) error {
+		return sender.Template.ExecuteTemplate(w, sender.TemplateName, templateData)
+	})
+
+	return m
+}
+
+func formatDescription(desc string) template.HTML {
+	escapedDesc := template.HTMLEscapeString(desc)
+	escapedDesc = strings.Replace(escapedDesc, "\n", "\n<br/>", -1)
+
+	return template.HTML(escapedDesc)
+}
+
+func (sender *Sender) dialAndSend(message *gomail.Message) error {
+	d := gomail.Dialer{
+		Host: sender.SMTPHost,
+		Port: int(sender.SMTPPort),
+		TLSConfig: &tls.Config{
+			InsecureSkipVerify: sender.InsecureTLS,
+			ServerName:         sender.SMTPHost,
+		},
+	}
+	if sender.Password != "" {
+		d.Auth = smtp.PlainAuth("", sender.Username, sender.Password, sender.SMTPHost)
+	}
+	if err := d.DialAndSend(message); err != nil {
+		return err
+	}
+	return nil
+}
---
+++ senders/mail/send_test.go
@@ -0,0 +1,64 @@
+package mail
+
+import (
+	"fmt"
+	"html/template"
+	"testing"
+
+	"time"
+
+	"github.com/moira-alert/moira"
+	"github.com/moira-alert/moira/logging/go-logging"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestMakeMessage(t *testing.T) {
+	logger, _ := logging.ConfigureLog("stdout", "debug", "test")
+	contact := moira.ContactData{
+		ID:    "ContactID-000000000000001",
+		Type:  "email",
+		Value: "mail1@example.com",
+	}
+
+	trigger := moira.TriggerData{
+		ID:         "triggerID-0000000000001",
+		Name:       "test trigger 1",
+		Targets:    []string{"test.target.1"},
+		WarnValue:  10,
+		ErrorValue: 20,
+		Tags:       []string{"test-tag-1"},
+	}
+
+	location, _ := time.LoadLocation("UTC")
+	templateName := "mail"
+
+	sender := Sender{
+		FrontURI:     "http://localhost",
+		From:         "test@notifier",
+		SMTPHost:     "localhost",
+		SMTPPort:     25,
+		Template:     template.Must(template.New(templateName).Parse(defaultTemplate)),
+		TemplateName: templateName,
+		location:     location,
+		logger:       logger,
+	}
+
+	Convey("Make message", t, func() {
+		message := sender.makeMessage(generateTestEvents(10, trigger.ID), contact, trigger, []byte{1, 0, 1}, true)
+		So(message.GetHeader("From")[0], ShouldEqual, sender.From)
+		So(message.GetHeader("To")[0], ShouldEqual, contact.Value)
+	})
+}
+
+func generateTestEvents(n int, subscriptionID string) []moira.NotificationEvent {
+	events := make([]moira.NotificationEvent, 0, n)
+	for i := 0; i < n; i++ {
+		event := moira.NotificationEvent{
+			Metric:         fmt.Sprintf("Metric number #%d", i),
+			SubscriptionID: &subscriptionID,
+			State:          "TEST",
+		}
+		events = append(events, event)
+	}
+	return events
+}
