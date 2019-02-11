591ef5150b664c61a11ce9f5b611c62b84f1e888
Borovsky Arkady <borovskyav@list.ru>
2019-02-11 17:37:00 +0500

    Move switch/case to run method

--- notifier/plotting.go
+++ notifier/plotting.go
@@ -56,12 +56,7 @@ func (notifier *StandardNotifier) buildNotificationPackagePlot(pkg NotificationP
 	notifier.logger.Debugf("rendering %s metricsData: %v", trigger.ID, metricsData)
 	renderable, err := plotTemplate.GetRenderable(trigger, metricsData)
 	if err != nil {
-		switch err.(type) {
-		case plotting.ErrNoPointsToRender:
-			return buff.Bytes(), nil
-		default:
-			return buff.Bytes(), err
-		}
+		return buff.Bytes(), err
 	}
 	if err = renderable.Render(chart.PNG, buff); err != nil {
 		return buff.Bytes(), err
