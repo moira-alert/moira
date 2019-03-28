e41cd24b65b9a5b4be3d384e1180c93cd80e1431
Borovsky Arkady <borovskyav@list.ru>
2019-03-28 22:13:02 +0500

    Update and create trigger now returns status code 400 on invalid request, add desription for Stop sending notifications log message (#323)
    * Add good desription for Stop sending notifications log message
    
    * Add custom error for api dto logical validations

--- api/dto/triggers_test.go
+++ api/dto/triggers_test.go
@@ -2,20 +2,21 @@ package dto
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"testing"
-	"fmt"
 
+	"github.com/moira-alert/moira"
+	"github.com/moira-alert/moira/api"
 	"github.com/moira-alert/moira/api/middleware"
 	"github.com/moira-alert/moira/metric_source"
 	"github.com/moira-alert/moira/mock/metric_source"
-	"github.com/moira-alert/moira"
 
 	"github.com/golang/mock/gomock"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
-func TestExpressionModeMultipleTargetsWarnValue (t *testing.T) {
+func TestExpressionModeMultipleTargetsWarnValue(t *testing.T) {
 
 	Convey("Tests targets, values and expression validation", t, func() {
 		mockCtrl := gomock.NewController(t)
@@ -34,7 +35,7 @@ func TestExpressionModeMultipleTargetsWarnValue (t *testing.T) {
 		request, _ := http.NewRequest("PUT", "/api/trigger", nil)
 		request.Header.Set("Content-Type", "application/json")
 		ctx := request.Context()
-		ctx = context.WithValue(ctx, middleware.ContextKey("metricSourceProvider"), sourceProvider )
+		ctx = context.WithValue(ctx, middleware.ContextKey("metricSourceProvider"), sourceProvider)
 		request = request.WithContext(ctx)
 
 		desc := "Graphite ClickHouse"
@@ -65,7 +66,7 @@ func TestExpressionModeMultipleTargetsWarnValue (t *testing.T) {
 					trigger.Expression = "(t1 < 10 && t2 < 10) ? WARN:OK"
 					tr := Trigger{trigger, throttling}
 					err := tr.Bind(request)
-					So(err, ShouldResemble, fmt.Errorf("can't use 'expression' to trigger_type: 'falling'"))
+					So(err, ShouldResemble, api.ErrInvalidRequestContent{ValidationError: fmt.Errorf("can't use 'expression' to trigger_type: 'falling'")})
 				})
 
 				Convey("and warn_value and error_value", func() {
@@ -88,7 +89,7 @@ func TestExpressionModeMultipleTargetsWarnValue (t *testing.T) {
 				trigger.ErrorValue = &errorValue
 				tr := Trigger{trigger, throttling}
 				err := tr.Bind(request)
-				So(err, ShouldResemble, fmt.Errorf("can't use trigger_type not 'falling' for with multiple targets"))
+				So(err, ShouldResemble, api.ErrInvalidRequestContent{ValidationError: fmt.Errorf("can't use trigger_type not 'falling' for with multiple targets")})
 			})
 
 		})
@@ -103,7 +104,7 @@ func TestExpressionModeMultipleTargetsWarnValue (t *testing.T) {
 					trigger.Expression = "(t1 < 10 && t2 < 10) ? WARN:OK"
 					tr := Trigger{trigger, throttling}
 					err := tr.Bind(request)
-					So(err, ShouldResemble, fmt.Errorf("can't use 'expression' to trigger_type: 'rising'"))
+					So(err, ShouldResemble, api.ErrInvalidRequestContent{ValidationError: fmt.Errorf("can't use 'expression' to trigger_type: 'rising'")})
 				})
 
 				Convey("and warn_value and error_value", func() {
@@ -126,7 +127,7 @@ func TestExpressionModeMultipleTargetsWarnValue (t *testing.T) {
 				trigger.ErrorValue = &warnValue
 				tr := Trigger{trigger, throttling}
 				err := tr.Bind(request)
-				So(err, ShouldResemble, fmt.Errorf("can't use trigger_type not 'rising' for with multiple targets"))
+				So(err, ShouldResemble, api.ErrInvalidRequestContent{ValidationError: fmt.Errorf("can't use trigger_type not 'rising' for with multiple targets")})
 			})
 
 		})
@@ -138,20 +139,20 @@ func TestExpressionModeMultipleTargetsWarnValue (t *testing.T) {
 				"aliasByNode(DevOps.system.sd2-graphite01.disk._mnt_data.gigabyte_percentfree, 2, 4)",
 				"aliasByNode(DevOps.system.bst-graphite01.disk.root.gigabyte_percentfree, 2, 4)",
 				"aliasByNode(DevOps.system.dtl-graphite01.disk._mnt_data.gigabyte_percentfree, 2, 4)",
-				}
+			}
 			Convey("and warn_value", func() {
 				trigger.WarnValue = &warnValue
 				tr := Trigger{trigger, throttling}
 				err := tr.Bind(request)
 				So(err, ShouldNotBeNil)
-				So(err, ShouldResemble, fmt.Errorf("can't use 'warn_value' on trigger_type: 'expression'"))
+				So(err, ShouldResemble, api.ErrInvalidRequestContent{ValidationError: fmt.Errorf("can't use 'warn_value' on trigger_type: 'expression'")})
 			})
 
 			Convey("and error_value", func() {
 				trigger.ErrorValue = &errorValue
 				tr := Trigger{trigger, throttling}
 				err := tr.Bind(request)
-				So(err, ShouldResemble, fmt.Errorf("can't use 'error_value' on trigger_type: 'expression'"))
+				So(err, ShouldResemble, api.ErrInvalidRequestContent{ValidationError: fmt.Errorf("can't use 'error_value' on trigger_type: 'expression'")})
 			})
 			Convey("and expression", func() {
 				tr := Trigger{trigger, throttling}
