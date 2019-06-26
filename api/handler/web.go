b9ecb34e13c85c68c56034f1d2978930d8667dd5
kamaev <timurkamaev@protonmail.com>
2019-06-26 15:31:28 +0500

    oO

--- api/handler/web.go
+++ api/handler/web.go
@@ -2,9 +2,9 @@ package handler
 
 import "net/http"
 
-func web(сonfigContent []byte) http.HandlerFunc {
+func web(configContent []byte) http.HandlerFunc {
 	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
 		writer.Header().Set("Content-Type", "application/json")
-		writer.Write(сonfigContent)
+		writer.Write(configContent)
 	})
 }
