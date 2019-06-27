2df9e4397198623c7c57197cebb3f26f4b1a78c1
Timur Kamaev <timurkamaev@protonmail.com>
2019-06-27 18:31:36 +0500

    Store both web & api configuration variables in the same file (api.yml) (#360)
    * add web config
    
    * /config
    
    * add help and examples
    
    * add empty contacts
    
    * fix desc
    
    * determine if remote is enabled
    
    * fix config and struct
    
    * fix compose configs
    
    * remove /web
    
    * config premarshal
    
    * oO
    
    * fix var names

---
+++ api/handler/config.go
@@ -0,0 +1,10 @@
+package handler
+
+import "net/http"
+
+func getWebConfig(configContent []byte) http.HandlerFunc {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		writer.Header().Set("Content-Type", "application/json")
+		writer.Write(configContent)
+	})
+}
