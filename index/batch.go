51b3b2558318a837d83585261d0202e30545c669
Nixolay <i.pro.linux@yandex.ru>
2019-07-24 19:07:55 +0500

    added tests for the actualizer_test.go and fixed handleTriggerBatches

--- index/batch.go
+++ index/batch.go
@@ -41,38 +41,36 @@ func (index *Index) getTriggerChecksBatches(triggerIDsBatches [][]string) (trigg
 func (index *Index) handleTriggerBatches(triggerChecksChan chan []*moira.TriggerCheck, getTriggersErrors chan error, toIndex int) error {
 	indexErrors := make(chan error)
 	wg := &sync.WaitGroup{}
+	defer wg.Wait()
 	var count int64
-	func() {
-		for {
-			select {
-			case batch, ok := <-triggerChecksChan:
-				if !ok {
+
+	for {
+		select {
+		case batch, ok := <-triggerChecksChan:
+			if !ok {
+				return nil
+			}
+			wg.Add(1)
+			go func(b []*moira.TriggerCheck) {
+				defer wg.Done()
+				err2 := index.triggerIndex.Write(b)
+				atomic.AddInt64(&count, int64(len(b)))
+				if err2 != nil {
+					indexErrors <- err2
 					return
 				}
-				wg.Add(1)
-				go func(b []*moira.TriggerCheck) {
-					defer wg.Done()
-					err2 := index.triggerIndex.Write(b)
-					atomic.AddInt64(&count, int64(len(b)))
-					if err2 != nil {
-						indexErrors <- err2
-						return
-					}
-					index.logger.Debugf("[%d triggers of %d] added to index", count, toIndex)
-				}(batch)
-			case err, ok := <-getTriggersErrors:
-				if ok {
-					index.logger.Errorf("Cannot get trigger checks from DB: %s", err.Error())
-				}
-				return
-			case err, ok := <-indexErrors:
-				if ok {
-					index.logger.Errorf("Cannot index trigger checks: %s", err.Error())
-				}
-				return
+				index.logger.Debugf("[%d triggers of %d] added to index", count, toIndex)
+			}(batch)
+		case err, ok := <-getTriggersErrors:
+			if ok {
+				index.logger.Errorf("Cannot get trigger checks from DB: %s", err.Error())
 			}
+			return err
+		case err, ok := <-indexErrors:
+			if ok {
+				index.logger.Errorf("Cannot index trigger checks: %s", err.Error())
+			}
+			return err
 		}
-	}()
-	wg.Wait()
-	return nil
+	}
 }
