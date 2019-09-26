06a56acc17e9c705f2aa2ddb5aba0d91479cdfa3
Ioannis Mavroukakis <imavroukakis@gmail.com>
2019-09-26 13:10:23 +0100

    move types to their own file

---
+++ senders/msteams/datatypes.go
@@ -0,0 +1,37 @@
+package msteams
+
+import "github.com/moira-alert/moira"
+
+type Fact struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
+	state moira.State
+}
+
+type Section struct {
+	ActivityTitle string `json:"activityTitle"`
+	ActivityText  string `json:"activityText"`
+	Facts         []Fact `json:"facts"`
+}
+
+type OpenUriTarget struct {
+	Os  string `json:"os"`
+	Uri string `json:"uri"`
+}
+
+//limited to OpenURI actions
+type Actions struct {
+	Type    string          `json:"@type"`
+	Name    string          `json:"name"`
+	Targets []OpenUriTarget `json:"targets"`
+}
+
+type MessageCard struct {
+	Context         string    `json:"@context"`
+	MessageType     string    `json:"@type"`
+	Summary         string    `json:"summary"`
+	ThemeColor      string    `json:"themeColor"`
+	Title           string    `json:"title"`
+	Sections        []Section `json:"sections"`
+	PotentialAction []Actions `json:"potentialAction"`
+}
