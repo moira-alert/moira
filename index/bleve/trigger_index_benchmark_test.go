6412a2f5e360c61d10e4167f10f069c64342c205
Borovsky Arkady <borovskyav@list.ru>
2020-03-17 17:28:05 +0500

    Add index benchmarks

--- index/bleve/trigger_index_benchmark_test.go
+++ index/bleve/trigger_index_benchmark_test.go
@@ -8,51 +8,122 @@ import (
 	"github.com/gofrs/uuid"
 	"github.com/moira-alert/moira"
 	"github.com/moira-alert/moira/index/mapping"
-	"github.com/op/go-logging"
 )
 
-type config struct {
-	triggersSize int
-	batchSize    int
-}
+func BenchmarkFillIndexFullBatch(b *testing.B) {
+	type config struct {
+		triggersSize int
+		batchSize    int
+	}
 
-var testCases = []config{
-	{50, 1000},
-	{100, 1000},
-	{250, 1000},
-	{500, 1000},
-	{1000, 1000},
-	{5000, 1000},
-	{10000, 1000},
-	{100, 10},
-	{500, 50},
-	{1000, 100},
-	{2500, 250},
-	{5000, 500},
-}
+	testCases := []config{
+		{50, 1000},
+		{100, 1000},
+		{250, 1000},
+		{500, 1000},
+		{1000, 1000},
+		{5000, 1000},
+		{10000, 1000},
+		{50000, 1000},
+		{100, 10},
+		{500, 50},
+		{1000, 100},
+		{2500, 250},
+		{5000, 500},
+	}
 
-func BenchmarkFillIndex(b *testing.B) {
 	for _, testCase := range testCases {
-		b.Run(fmt.Sprintf("BenchmarkFillIndex_Size:%d_Batch:%d", testCase.triggersSize, testCase.batchSize),
+		b.Run(fmt.Sprintf("BenchmarkFillIndexFullBatch_Size:%d_Batch:%d", testCase.triggersSize, testCase.batchSize),
 			func(b *testing.B) {
-				runBenchmark(b, testCase.triggersSize, testCase.batchSize)
+				triggersSize := testCase.triggersSize
+				batchSize := testCase.batchSize
+
+				triggersPointers := generateTriggerChecks(triggersSize)
+				triggerMapping := mapping.BuildIndexMapping(mapping.Trigger{})
+
+				b.ResetTimer()
+				b.ReportAllocs()
+
+				for n := 0; n < b.N; n++ {
+					newIndex, _ := CreateTriggerIndex(triggerMapping)
+					fillIndexWithTriggers(newIndex, triggersPointers, batchSize)
+				}
 			})
 	}
 }
 
-func runBenchmark(b *testing.B, triggersSize int, batchSize int) {
-	logger, _ := logging.GetLogger("Benchmark")
-	triggersPointers := generateTriggerChecks(triggersSize)
-	triggerMapping := mapping.BuildIndexMapping(mapping.Trigger{})
+func BenchmarkFillAlwaysEmptyIndex(b *testing.B) {
+	testCases := []int{50, 100, 300, 500, 1000, 3000, 5000, 10000}
 
-	b.ResetTimer()
-	b.ReportAllocs()
+	for _, batchSize := range testCases {
+		b.Run(fmt.Sprintf("BenchmarkFillAlwaysEmptyIndex_Batch:%d", batchSize),
+			func(b *testing.B) {
+				triggersPointers := generateTriggerChecks(batchSize)
+				triggerMapping := mapping.BuildIndexMapping(mapping.Trigger{})
 
-	for n := 0; n < b.N; n++ {
-		newIndex, _ := CreateTriggerIndex(triggerMapping)
-		logger.Infof("[Benchmark] [BatchSize: %d] [Triggers: %d] Start", batchSize, triggersSize)
-		fillIndexWithTriggers(newIndex, triggersPointers, batchSize)
-		logger.Infof("[Benchmark] [BatchSize: %d] [Triggers: %d] Finish", batchSize, triggersSize)
+				b.ResetTimer()
+				b.ReportAllocs()
+
+				for n := 0; n < b.N; n++ {
+					newIndex, _ := CreateTriggerIndex(triggerMapping)
+					fillIndexWithTriggers(newIndex, triggersPointers, batchSize)
+				}
+			})
+	}
+}
+
+func BenchmarkFillAlreadyFilledIndex(b *testing.B) {
+	testCases := []int{50, 100, 300, 500, 1000}
+
+	for _, batchSize := range testCases {
+		b.Run(fmt.Sprintf("BenchmarkFillAlreadyFilledIndex_Batch:%d", batchSize),
+			func(b *testing.B) {
+				triggersPointers := generateTriggerChecks(batchSize)
+				triggerMapping := mapping.BuildIndexMapping(mapping.Trigger{})
+
+				b.ResetTimer()
+				b.ReportAllocs()
+
+				for n := 0; n < b.N; n++ {
+					newIndex, _ := CreateTriggerIndex(triggerMapping)
+					fillIndexWithTriggers(newIndex, triggersPointers, batchSize)
+				}
+			})
+	}
+}
+
+func BenchmarkEmptySearch(b *testing.B) {
+	type config struct {
+		triggersCount int
+		pageSize      int64
+	}
+
+	testCases := []config{
+		{50, 10},
+		{100, 10},
+		{250, 10},
+		{500, 10},
+		{1000, 10},
+		{5000, 10},
+		{10000, 10},
+		{50000, 10},
+	}
+
+	for _, testCase := range testCases {
+		b.Run(fmt.Sprintf("BenchmarkEmptySearch_TriggersCount:%d_PageSize_%d", testCase.triggersCount, testCase.pageSize),
+			func(b *testing.B) {
+				triggersPointers := generateTriggerChecks(testCase.triggersCount)
+				triggerMapping := mapping.BuildIndexMapping(mapping.Trigger{})
+				newIndex, _ := CreateTriggerIndex(triggerMapping)
+				fillIndexWithTriggers(newIndex, triggersPointers, testCase.triggersCount)
+
+				b.ResetTimer()
+				b.ReportAllocs()
+
+				for n := 0; n < b.N; n++ {
+					newIndex.Search(make([]string, 0), "", false, 0, testCase.pageSize)
+				}
+			})
 	}
 }
 
