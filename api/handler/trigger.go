1814e0aff40df52f84d1f394d7964fb5a5da0649
Nikolay <n.kudrin@kontur.ru>
2020-04-08 20:26:40 +0500

    feat: Add templates to trigger description #484
    * Added templates to trigger description
    * Added presentation of trigger description in non-editing for api
    
    Closed #484

--- api/handler/trigger.go
+++ api/handler/trigger.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/render"
+	"github.com/moira-alert/moira"
 	"github.com/moira-alert/moira/metric_source/local"
 	"github.com/moira-alert/moira/metric_source/remote"
 
@@ -20,7 +21,8 @@ import (
 func trigger(router chi.Router) {
 	router.Use(middleware.TriggerContext)
 	router.Put("/", updateTrigger)
-	router.Get("/", getTrigger)
+	router.With(middleware.TriggerContext,
+		middleware.Populate(false)).Get("/", getTrigger)
 	router.Delete("/", removeTrigger)
 	router.Get("/state", getTriggerState)
 	router.Route("/throttling", func(router chi.Router) {
@@ -35,6 +37,7 @@ func trigger(router chi.Router) {
 func updateTrigger(writer http.ResponseWriter, request *http.Request) {
 	triggerID := middleware.GetTriggerID(request)
 	trigger := &dto.Trigger{}
+
 	if err := render.Bind(request, trigger); err != nil {
 		switch err := err.(type) {
 		case local.ErrParseExpr, local.ErrEvalExpr, local.ErrUnknownFunction:
@@ -50,7 +53,17 @@ func updateTrigger(writer http.ResponseWriter, request *http.Request) {
 		default:
 			render.Render(writer, request, api.ErrorInternalServer(err))
 		}
+
 		return
+	}
+
+	if trigger.Desc != nil {
+		triggerData := moira.TriggerData{Desc: *trigger.Desc, Name: trigger.Name}
+		if _, err := triggerData.GetPopulatedDescription(moira.NotificationEvents{}); err != nil {
+			render.Render(writer, request, api.ErrorRender(
+				fmt.Errorf("You have an error in your Go template: %v", err)))
+			return
+		}
 	}
 
 	timeSeriesNames := middleware.GetTimeSeriesNames(request)
@@ -84,6 +97,18 @@ func getTrigger(writer http.ResponseWriter, request *http.Request) {
 		render.Render(writer, request, err)
 		return
 	}
+
+	if needToPopulate := middleware.GetPopulated(request); needToPopulate && trigger.Desc != nil {
+		triggerData := moira.TriggerData{Desc: *trigger.Desc, Name: trigger.Name}
+
+		eventsList, err := controller.GetTriggerEvents(database, triggerID, 0, 3)
+		if err != nil {
+			render.Render(writer, request, err)
+		}
+
+		*trigger.Desc, _ = triggerData.GetPopulatedDescription(eventsList.List)
+	}
+
 	if err := render.Render(writer, request, trigger); err != nil {
 		render.Render(writer, request, api.ErrorRender(err))
 	}
--- notifier/notifier.go
+++ notifier/notifier.go
@@ -160,6 +160,12 @@ func (notifier *StandardNotifier) runSender(sender moira.Sender, ch chan Notific
 				notifier.logger.Errorf(buildErr)
 			}
 		}
+
+		pkg.Trigger.Desc, err = pkg.Trigger.GetPopulatedDescription(pkg.Events)
+		if err != nil {
+			notifier.logger.Errorf("Error populate description: %v", err)
+		}
+
 		err = sender.SendEvents(pkg.Events, pkg.Contact, pkg.Trigger, plot, pkg.Throttled)
 		if err == nil {
 			if metric, found := notifier.metrics.SendersOkMetrics.GetRegisteredMeter(pkg.Contact.Type); found {
