09f6743839543860e7b768665d8a096459d6e55d
Borovsky Arkady <borovskyav@list.ru>
2020-06-10 23:41:46 +0500

    fix tests

--- senders/discord/send_test.go
+++ senders/discord/send_test.go
@@ -197,6 +197,7 @@ aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
 02:40: Metric name = 97.4458331200185 (OK to NODATA)
 02:40: Metric name = 97.4458331200185 (OK to NODATA)
 02:40: Metric name = 97.4458331200185 (OK to NODATA)
+02:40: Metric name = 97.4458331200185 (OK to NODATA)
 
 ...and 4 more events.`
 
