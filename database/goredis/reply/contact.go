4081b2f6f376451414ac8c9dee8655404aa1047b
Максунов Артём Дмитриевич <maksunov@skbkontur.ru>
2021-09-03 01:12:37 +0500

    refactor(redis): Move from redigo to go-redis library (notifications)

--- database/goredis/reply/contact.go
+++ database/goredis/reply/contact.go
@@ -11,7 +11,7 @@ import (
 )
 
 // Contact converts redis DB reply to moira.ContactData object
-func Contact(rep redis.StringCmd) (moira.ContactData, error) {
+func Contact(rep *redis.StringCmd) (moira.ContactData, error) {
 	contact := moira.ContactData{}
 	bytes, err := rep.Bytes()
 	if err != nil {
@@ -20,9 +20,27 @@ func Contact(rep redis.StringCmd) (moira.ContactData, error) {
 		}
 		return contact, fmt.Errorf("failed to read contact: %s", err.Error())
 	}
+
 	err = json.Unmarshal(bytes, &contact)
 	if err != nil {
 		return contact, fmt.Errorf("failed to parse contact json %s: %s", string(bytes), err.Error())
 	}
+
 	return contact, nil
+}
+
+// Contacts converts redis DB reply to moira.ContactData objects array
+func Contacts(rep []*redis.StringCmd) ([]*moira.ContactData, error) {
+	contacts := make([]*moira.ContactData, len(rep))
+	for i, value := range rep {
+		contact, err2 := Contact(value)
+		if err2 != nil && err2 != database.ErrNil {
+			return nil, err2
+		} else if err2 == database.ErrNil {
+			contacts[i] = nil
+		} else {
+			contacts[i] = &contact
+		}
+	}
+	return contacts, nil
 }
