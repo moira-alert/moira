e43c03bb2da80508252d980f4f352f2b2ffff2e2
m_kiskachi <m_kiskachi@skbkontur.ru>
2021-09-06 17:35:09 +0500

    refactor: Move from redigo to go-redis library (subscription)

--- database/goredis/subscription_test.go
+++ database/goredis/subscription_test.go
@@ -306,7 +306,7 @@ func TestSubscriptionData(t *testing.T) {
 
 func TestSubscriptionErrorConnection(t *testing.T) {
 	logger, _ := logging.GetLogger("dataBase")
-	dataBase := newTestDatabase(logger, wrongConfig)
+	dataBase := newTestDatabase(logger, incorrectConfig)
 	dataBase.flush()
 	defer dataBase.flush()
 	Convey("Should throw error when no connection", t, func() {
