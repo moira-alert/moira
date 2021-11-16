2d76be026962f7e7f7e974a6c7c124e61891fea5
d.anchikov <d.anchikov@skbkontur.ru>
2021-11-16 16:37:30 +0400

    test(filter): Updated tests per feedback

--- perfomance_tests/filter/filter_plain_metrics_test.go
+++ perfomance_tests/filter/filter_plain_metrics_test.go
@@ -1,61 +1,31 @@
 package filter
 
 import (
-	"bufio"
-	"fmt"
-	"io"
 	"math/rand"
-	"os"
 	"strings"
 	"testing"
 	"time"
 
-	"github.com/golang/mock/gomock"
 	"github.com/moira-alert/moira/filter"
-	logging "github.com/moira-alert/moira/logging/zerolog_adapter"
-	"github.com/moira-alert/moira/metrics"
-	mock_moira_alert "github.com/moira-alert/moira/mock/moira-alert"
 )
 
-func BenchmarkProcessIncomingMetric(b *testing.B) {
-	patternsTxt, err := os.Open("patterns.txt")
+func BenchmarkProcessIncomingPlainMetric(b *testing.B) {
+	plainPatterns, err := loadPatterns("plain_patterns.txt")
 	if err != nil {
 		b.Errorf(err.Error())
 	}
-	patterns := make([]string, 0)
-	patternsReader := bufio.NewReader(patternsTxt)
-	for {
-		pattern, _, err1 := patternsReader.ReadLine()
-		if err1 != nil {
-			break
-		}
-		patterns = append(patterns, string(pattern))
-	}
-	if err != nil && err != io.EOF {
-		b.Errorf("Error reading patterns: %s", err.Error())
-	}
 
-	filterMetrics := metrics.ConfigureFilterMetrics(metrics.NewDummyRegistry())
-
-	mockCtrl := gomock.NewController(b)
-	database := mock_moira_alert.NewMockDatabase(mockCtrl)
-	database.EXPECT().AllowStale().AnyTimes().Return(database)
-	logger, _ := logging.GetLogger("Benchmark")
-
-	database.EXPECT().GetPatterns().Return(patterns, nil)
-	patternsStorage, err := filter.NewPatternStorage(database, filterMetrics, logger)
+	patternsStorage, err := createPatternsStorage(plainPatterns, b)
 	if err != nil {
-		b.Errorf("Can not create new cache storage %s", err)
+		b.Errorf("Can not create new patterns storage %s", err)
 	}
+
 	testMetricsLines := generateMetrics(patternsStorage, b.N)
 
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		patternsStorage.ProcessIncomingMetric([]byte(testMetricsLines[i]))
-	}
+	runBenchmark(b, patternsStorage, testMetricsLines)
 }
 
-func generateMetrics(patternStorage *filter.PatternStorage, count int) []string {
+func generateMetrics(patternStorage *filter.PatternStorage, count int) *[]string {
 	result := make([]string, 0, count)
 	timestamp := time.Now()
 	patternTree := patternStorage.PatternIndex.Load().(*filter.PatternIndex).Root
@@ -80,24 +50,11 @@ func generateMetrics(patternStorage *filter.PatternStorage, count int) []string 
 			level += 0.7
 			node = node.Children[rand.Intn(len(node.Children))]
 		}
-		value := rand.Float32()
-		ts := fmt.Sprintf("%d", timestamp.Unix())
-		v := fmt.Sprintf("%f", value)
-		path := strings.Join(parts, ".")
-		metric := strings.Join([]string{path, v, ts}, " ")
+		matchedMetricPath := strings.Join(parts, ".")
+		metric := generateMetricLineByPath(matchedMetricPath, timestamp)
 		result = append(result, metric)
 		timestamp = timestamp.Add(time.Microsecond)
 	}
 
-	return result
-}
-
-const letterBytes = "abcdefghijklmnopqrstuvwxyz"
-
-func RandStringBytes(n int) string {
-	b := make([]byte, n)
-	for i := range b {
-		b[i] = letterBytes[rand.Intn(len(letterBytes))]
-	}
-	return string(b)
+	return &result
 }
--- perfomance_tests/filter/filter_tagged_metrics_test.go
+++ perfomance_tests/filter/filter_tagged_metrics_test.go
@@ -1,95 +1,96 @@
 package filter
 
 import (
-	"bufio"
 	"fmt"
 	"math/rand"
-	"os"
 	"strings"
 	"testing"
 	"time"
 
-	"github.com/golang/mock/gomock"
 	"github.com/moira-alert/moira/filter"
-	logging "github.com/moira-alert/moira/logging/zerolog_adapter"
-	"github.com/moira-alert/moira/metrics"
-	mock_moira_alert "github.com/moira-alert/moira/mock/moira-alert"
 )
 
 func BenchmarkProcessIncomingTaggedMetric(b *testing.B) {
-	taggedPatterns, err := loadTaggedPatterns()
+	taggedPatterns, err := loadPatterns("tagged_patterns.txt")
 	if err != nil {
 		b.Errorf(err.Error())
 	}
-	filterMetrics := metrics.ConfigureFilterMetrics(metrics.NewDummyRegistry())
 
-	mockCtrl := gomock.NewController(b)
-	database := mock_moira_alert.NewMockDatabase(mockCtrl)
-	database.EXPECT().AllowStale().AnyTimes().Return(database)
-	logger, _ := logging.GetLogger("Benchmark")
-
-	database.EXPECT().GetPatterns().Return(taggedPatterns, nil)
-	patternsStorage, err := filter.NewPatternStorage(database, filterMetrics, logger)
+	patternsStorage, err := createPatternsStorage(taggedPatterns, b)
 	if err != nil {
-		b.Errorf("Can not create new cache storage %s", err)
+		b.Errorf("Can not create new patterns storage %s", err)
 	}
-	testMetricsLines := generateTaggedMetrics(&taggedPatterns, b.N)
 
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		patternsStorage.ProcessIncomingMetric([]byte(testMetricsLines[i]))
-	}
+	testMetricsLines := generateTaggedMetrics(taggedPatterns, b.N)
+
+	runBenchmark(b, patternsStorage, testMetricsLines)
 }
 
-func loadTaggedPatterns() ([]string, error) {
-	taggedPatternsTxt, err := os.Open("tagged_patterns.txt")
-	if err != nil {
-		return nil, err
-	}
-	taggedPatterns := make([]string, 0)
-	patternsReader := bufio.NewReader(taggedPatternsTxt)
-	lastLineWasPrefixed := false
-	for {
-		pattern, isPrefix, err1 := patternsReader.ReadLine()
-		if err1 != nil {
-			break
-		}
-		if lastLineWasPrefixed {
-			taggedPatterns[len(taggedPatterns)-1] = fmt.Sprintf(
-				"%s%s", taggedPatterns[len(taggedPatterns)-1], pattern,
-			)
-		} else {
-			taggedPatterns = append(taggedPatterns, string(pattern))
-		}
-		lastLineWasPrefixed = isPrefix
-	}
-	return taggedPatterns, nil
-}
-func generateTaggedMetrics(taggedPatterns *[]string, count int) []string {
-	result := make([]string, 0, count)
+func generateTaggedMetrics(taggedPatterns *[]string, count int) *[]string {
+	result := make([]string, 0, 3*count)
 	timestamp := time.Now()
 
 	for i := 0; i < count; i++ {
 		taggedPattern := (*taggedPatterns)[rand.Intn(len(*taggedPatterns))]
 		tagSpecs, _ := filter.ParseSeriesByTag(taggedPattern)
-		matchedMetricPathParts := make([]string, 0)
-		for _, tagSpec := range tagSpecs {
-			tagValues := strings.Split(tagSpec.Value, "|")
-			tagValue := tagValues[rand.Intn(len(tagValues))]
-			if tagSpec.Name == "name" {
-				matchedMetricPathParts = append([]string{tagValue}, matchedMetricPathParts...)
-			} else {
-				matchedMetricPathParts = append(matchedMetricPathParts, fmt.Sprintf("%s=%s", tagSpec.Name, tagValue))
-			}
-		}
-		matchedMetricPath := strings.Join(matchedMetricPathParts, ";")
-		value := rand.Float32()
-		ts := fmt.Sprintf("%d", timestamp.Unix())
-		v := fmt.Sprintf("%f", value)
-		metric := strings.Join([]string{matchedMetricPath, v, ts}, " ")
-		result = append(result, metric)
+
+		matchedMetric := generateMatchedMetric(tagSpecs, timestamp.Add(time.Microsecond))
+		partiallyMatchedMetric := generatePartiallyMatchedMetric(tagSpecs, timestamp.Add(time.Microsecond))
+		notMatchedMetric := generateNotMatchedMetric(tagSpecs, timestamp.Add(time.Microsecond))
+
+		result = append(result, matchedMetric, partiallyMatchedMetric, notMatchedMetric)
 		timestamp = timestamp.Add(time.Microsecond)
 	}
 
-	return result
+	return &result
+}
+
+func generateMatchedMetric(tagSpecs []filter.TagSpec, timestamp time.Time) string {
+	pathParts := make([]string, 0)
+	for _, tagSpec := range tagSpecs {
+		tagValues := strings.Split(tagSpec.Value, "|")
+		tagValue := tagValues[rand.Intn(len(tagValues))]
+		pathParts = addTag(pathParts, tagSpec, tagValue)
+	}
+	metricPath := strings.Join(pathParts, ";")
+	return generateMetricLineByPath(metricPath, timestamp)
+}
+
+func generatePartiallyMatchedMetric(tagSpecs []filter.TagSpec, timestamp time.Time) string {
+	// there will be only one matched tag
+	matchedTag := tagSpecs[rand.Intn(len(tagSpecs))]
+	randomTagValueLength := 5
+
+	pathParts := make([]string, 0)
+	for _, tagSpec := range tagSpecs {
+		var tagValue string
+		if tagSpec.Name == matchedTag.Name {
+			tagValues := strings.Split(tagSpec.Value, "|")
+			tagValue = tagValues[rand.Intn(len(tagValues))]
+		} else {
+			tagValue = RandStringBytes(randomTagValueLength)
+		}
+		pathParts = addTag(pathParts, tagSpec, tagValue)
+	}
+	metricPath := strings.Join(pathParts, ";")
+	return generateMetricLineByPath(metricPath, timestamp)
+}
+
+func generateNotMatchedMetric(tagSpecs []filter.TagSpec, timestamp time.Time) string {
+	randomTagValueLength := 5
+	pathParts := make([]string, 0)
+	for _, tagSpec := range tagSpecs {
+		tagValue := RandStringBytes(randomTagValueLength)
+		pathParts = addTag(pathParts, tagSpec, tagValue)
+	}
+	metricPath := strings.Join(pathParts, ";")
+	return generateMetricLineByPath(metricPath, timestamp)
+}
+
+func addTag(pathParts []string, tagSpec filter.TagSpec, tagValue string) []string {
+	if tagSpec.Name == "name" {
+		// name tag should be prepended
+		return append([]string{tagValue}, pathParts...)
+	}
+	return append(pathParts, fmt.Sprintf("%s=%s", tagSpec.Name, tagValue))
 }
