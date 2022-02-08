7c8e39250b591c813d8e25aa1a1d66861af2e44a
m_kiskachi <m_kiskachi@skbkontur.ru>
2022-02-08 17:26:49 +0500

    refactor(filter): Payload with matcher handlers moved to tree.

---
+++ filter/prefix_tree.go
@@ -0,0 +1,203 @@
+package filter
+
+import (
+	"path"
+	"strings"
+
+	"github.com/cespare/xxhash/v2"
+	"github.com/moira-alert/moira"
+)
+
+var asteriskHash = xxhash.Sum64String("*")
+
+type PrefixTree struct {
+	Root   *PatternNode
+	Logger moira.Logger
+}
+
+// PatternNode contains pattern node
+type PatternNode struct {
+	Children   []*PatternNode
+	Part       string
+	Hash       uint64
+	Prefix     string
+	InnerParts []string
+	Terminal   bool
+	Payload    map[string]MatchingHandler
+}
+
+// Add inserts pattern in tree
+func (source *PrefixTree) Add(pattern string) {
+	source.AddWithPayload(pattern, "", nil)
+}
+
+// AddWithPayload inserts pattern and payload in tree
+func (source *PrefixTree) AddWithPayload(pattern string, payloadKey string, payloadValue MatchingHandler) {
+	currentNode := source.Root
+	parts := strings.Split(pattern, ".")
+	if hasEmptyParts(parts) {
+		source.Logger.Warningf("Pattern %s is ignored because it contains an empty part", pattern)
+		return
+	}
+	for i, part := range parts {
+		found := false
+		for _, child := range currentNode.Children {
+			if part == child.Part {
+				currentNode = child
+				found = true
+				break
+			}
+		}
+		if !found {
+			newNode := &PatternNode{Part: part}
+
+			if currentNode.Prefix == "" {
+				newNode.Prefix = part
+			} else {
+				newNode.Prefix = currentNode.Prefix + "." + part
+			}
+
+			if part == "*" || !strings.ContainsAny(part, "{*?") {
+				newNode.Hash = xxhash.Sum64String(part)
+			} else {
+				if strings.Contains(part, "{") && strings.Contains(part, "}") {
+					prefix, bigSuffix := split2(part, "{")
+					inner, suffix := split2(bigSuffix, "}")
+					innerParts := strings.Split(inner, ",")
+
+					newNode.InnerParts = make([]string, 0, len(innerParts))
+					for _, innerPart := range innerParts {
+						whole := prefix + innerPart + suffix
+						newNode.InnerParts = append(newNode.InnerParts, whole)
+					}
+				} else {
+					newNode.InnerParts = []string{part}
+				}
+			}
+			currentNode.Children = append(currentNode.Children, newNode)
+			currentNode = newNode
+		}
+		if i == len(parts)-1 {
+			currentNode.Terminal = true
+			if payloadValue != nil {
+				if currentNode.Payload == nil {
+					currentNode.Payload = make(map[string]MatchingHandler)
+				}
+				currentNode.Payload[payloadKey] = payloadValue
+			}
+		}
+	}
+}
+
+// Match finds metric in tree and returns prefixes for all matched nodes
+func (source *PrefixTree) Match(metric string) []string {
+	nodes, found := source.findNodes(metric)
+	if found == 0 {
+		return []string{}
+	}
+
+	matched := make([]string, 0, found)
+	for _, node := range nodes {
+		if node.Terminal {
+			matched = append(matched, node.Prefix)
+		}
+	}
+
+	return matched
+}
+
+// MatchWithValue finds metric in tree and returns payloads for all matched nodes
+func (source *PrefixTree) MatchWithValue(metric string) map[string]MatchingHandler {
+	nodes, _ := source.findNodes(metric)
+	if nodes == nil {
+		return map[string]MatchingHandler{}
+	}
+
+	matched := make(map[string]MatchingHandler)
+	for _, node := range nodes {
+		if node.Terminal {
+			if node.Payload == nil {
+				matched[node.Prefix] = nil
+			}
+			for pattern, matchingHandler := range node.Payload {
+				matched[pattern] = matchingHandler
+			}
+		}
+	}
+
+	return matched
+}
+
+func (source *PrefixTree) findNodes(metric string) ([]*PatternNode, int) {
+	currentLevel := []*PatternNode{source.Root}
+	var found, index int
+	for i, c := range metric {
+		if c == '.' {
+			part := metric[index:i]
+
+			if len(part) == 0 {
+				source.Logger.Warningf("Metric %s is ignored, because it contains empty parts", metric)
+				return nil, 0
+			}
+
+			index = i + 1
+
+			currentLevel, found = findPart(part, currentLevel)
+			if found == 0 {
+				return nil, 0
+			}
+		}
+	}
+
+	part := metric[index:]
+	currentLevel, found = findPart(part, currentLevel)
+	if found == 0 {
+		return nil, 0
+	}
+	return currentLevel, found
+}
+
+func findPart(part string, currentLevel []*PatternNode) ([]*PatternNode, int) {
+	nextLevel := make([]*PatternNode, 0, 5)
+
+	hash := xxhash.Sum64String(part)
+	for _, node := range currentLevel {
+		for _, child := range node.Children {
+			match := false
+
+			if child.Hash == asteriskHash || child.Hash == hash {
+				match = true
+			} else if len(child.InnerParts) > 0 {
+				for _, innerPart := range child.InnerParts {
+					innerMatch, _ := path.Match(innerPart, part)
+					if innerMatch {
+						match = true
+						break
+					}
+				}
+			}
+
+			if match {
+				nextLevel = append(nextLevel, child)
+			}
+		}
+	}
+	return nextLevel, len(nextLevel)
+}
+
+func split2(s, sep string) (string, string) {
+	splitResult := strings.SplitN(s, sep, 2)
+	if len(splitResult) < 2 { //nolint
+		return splitResult[0], ""
+	}
+	return splitResult[0], splitResult[1]
+}
+
+func hasEmptyParts(parts []string) bool {
+	for _, part := range parts {
+		if part == "" {
+			return true
+		}
+	}
+	return false
+}
--- filter/series_by_tag.go
+++ filter/series_by_tag.go
@@ -3,8 +3,6 @@ package filter
 import (
 	"fmt"
 	"regexp"
-
-	"github.com/moira-alert/moira"
 )
 
 var tagSpecRegex = regexp.MustCompile(`^["']([^,!=]+)\s*(!?=~?)\s*([^,]*)["']`)
@@ -74,7 +72,35 @@ func ParseSeriesByTag(input string) ([]TagSpec, error) {
 	return tagSpecs, nil
 }
 
-func createMatchingHandler(spec TagSpec) MatchingHandler {
+// MatchingHandler is a function for pattern matching
+type MatchingHandler func(string, map[string]string) bool
+
+// CreateMatchingHandlerForPattern creates function for matching by tag list
+func CreateMatchingHandlerForPattern(tagSpecs []TagSpec) (string, MatchingHandler) {
+	matchingHandlers := make([]MatchingHandler, 0)
+	var nameTagValue string
+
+	for _, tagSpec := range tagSpecs {
+		if tagSpec.Name == "name" && tagSpec.Operator == EqualOperator {
+			nameTagValue = tagSpec.Value
+		} else {
+			matchingHandlers = append(matchingHandlers, createMatchingHandlerForOneTag(tagSpec))
+		}
+	}
+
+	matchingHandler := func(metric string, labels map[string]string) bool {
+		for _, matchingHandler := range matchingHandlers {
+			if !matchingHandler(metric, labels) {
+				return false
+			}
+		}
+		return true
+	}
+
+	return nameTagValue, matchingHandler
+}
+
+func createMatchingHandlerForOneTag(spec TagSpec) MatchingHandler {
 	var matchingHandlerCondition func(string) bool
 	allowMatchEmpty := false
 	switch spec.Operator {
@@ -114,105 +140,4 @@ func createMatchingHandler(spec TagSpec) MatchingHandler {
 		}
 		return allowMatchEmpty && matchEmpty
 	}
-}
-
-type MatchingHandler func(string, map[string]string) bool
-
-// SeriesByTagPatternIndex helps to index the seriesByTag patterns and allows to match them by metric
-type SeriesByTagPatternIndex struct {
-	// namesPatternIndex stores name tag values
-	namesPatternIndex *PatternIndex
-	// nameToPatternMatchers stores MatchingHandler's for name tag values
-	nameToPatternMatchers map[string]map[string]MatchingHandler
-	// withoutStrictNameTagPatternMatchers stores MatchingHandler's for patterns that have no name tag
-	withoutStrictNameTagPatternMatchers map[string]MatchingHandler
-}
-
-// NewSeriesByTagPatternIndex creates new SeriesByTagPatternIndex using seriesByTag patterns and parsed specs comes from ParseSeriesByTag
-func NewSeriesByTagPatternIndex(logger moira.Logger, tagSpecsByPattern map[string][]TagSpec) *SeriesByTagPatternIndex {
-	names := make(map[string]bool)
-	nameToPatternMatchers := make(map[string]map[string]MatchingHandler)
-	withoutStrictNameTagPatternMatchers := make(map[string]MatchingHandler)
-
-	for pattern, tagSpecs := range tagSpecsByPattern {
-		matchingHandlers := make([]MatchingHandler, 0)
-		var nameTagValue string
-
-		for _, tagSpec := range tagSpecs {
-			if tagSpec.Name == "name" && tagSpec.Operator == EqualOperator {
-				nameTagValue = tagSpec.Value
-				names[nameTagValue] = true
-			} else {
-				matchingHandlers = append(matchingHandlers, createMatchingHandler(tagSpec))
-			}
-		}
-
-		matchingHandler := func(metric string, labels map[string]string) bool {
-			for _, matchingHandler := range matchingHandlers {
-				if !matchingHandler(metric, labels) {
-					return false
-				}
-			}
-			return true
-		}
-
-		// Splitting all patterns into two maps: nameToPatternMatchers stores patterns with strict name tag and
-		// withoutStrictNameTagPatternMatchers stores other patterns
-		if nameTagValue == "" {
-			withoutStrictNameTagPatternMatchers[pattern] = matchingHandler
-		} else {
-			if _, ok := nameToPatternMatchers[nameTagValue]; ok {
-				nameToPatternMatchers[nameTagValue][pattern] = matchingHandler
-			} else {
-				nameToPatternMatchers[nameTagValue] = map[string]MatchingHandler{pattern: matchingHandler}
-			}
-		}
-	}
-
-	namePatternIndex := NewPatternIndex(logger, getMapKeys(names))
-	return &SeriesByTagPatternIndex{
-		namesPatternIndex:                   namePatternIndex,
-		nameToPatternMatchers:               nameToPatternMatchers,
-		withoutStrictNameTagPatternMatchers: withoutStrictNameTagPatternMatchers}
-}
-
-// MatchPatterns allows to match patterns by metric name and its labels
-func (index *SeriesByTagPatternIndex) MatchPatterns(name string, labels map[string]string) []string {
-	matchedPatterns := make([]string, 0)
-
-	matchingHandlersWithCorrespondingNameTag := getMatchingHandlersWithCorrespondingNameTag(index, name)
-	for pattern, matchingHandler := range matchingHandlersWithCorrespondingNameTag {
-		if matchingHandler(name, labels) {
-			matchedPatterns = append(matchedPatterns, pattern)
-		}
-	}
-
-	for pattern, matchingHandler := range index.withoutStrictNameTagPatternMatchers {
-		if (matchingHandler)(name, labels) {
-			matchedPatterns = append(matchedPatterns, pattern)
-		}
-	}
-
-	return matchedPatterns
-}
-
-func getMatchingHandlersWithCorrespondingNameTag(index *SeriesByTagPatternIndex, metricName string) map[string]MatchingHandler {
-	patternsToMatchingHandlers := make(map[string]MatchingHandler)
-	patternNames := index.namesPatternIndex.MatchPatterns(metricName)
-	for _, patternName := range patternNames {
-		for pattern, matchingHandlers := range index.nameToPatternMatchers[patternName] {
-			patternsToMatchingHandlers[pattern] = matchingHandlers
-		}
-	}
-	return patternsToMatchingHandlers
-}
-
-func getMapKeys(dict map[string]bool) []string {
-	keys := make([]string, len(dict))
-	i := 0
-	for key := range dict {
-		keys[i] = key
-		i++
-	}
-	return keys
 }
