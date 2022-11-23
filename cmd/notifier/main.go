6561f9dde457296b2307cd5872e5e5193740ccf0
Dmitry Redkin <red.dn@skbkontur.ru>
2022-11-23 11:16:31 +0500

    remove useless .Error() call

--- cmd/notifier/main.go
+++ cmd/notifier/main.go
@@ -149,7 +149,7 @@ func stopNotificationsFetcher(worker *notifications.FetchNotificationsWorker) {
 func stopSelfStateChecker(checker *selfstate.SelfCheckWorker) {
 	if err := checker.Stop(); err != nil {
 		if errors.Is(err, selfstate.ErrDisabled) {
-			logger.Warning(err.Error())
+			logger.Warning(err)
 		} else {
 			logger.Errorf("Failed to stop self check worker: %v", err)
 		}
