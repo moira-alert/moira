50863133ec96fa1b26a5b0b61457504a6e61c8f5
Xenia Nisskhen <nisskhen.kd@skbkontur.ru>
2023-06-14 12:38:29 +0600

    feat(cli): add func to delete unused triggers without subscriptions

--- cmd/cli/triggers.go
+++ cmd/cli/triggers.go
@@ -7,14 +7,43 @@ import (
 	"github.com/moira-alert/moira"
 )
 
+// Added delay because command is potentially dangerous and can delete unwanted triggers
+var delay = 10 * time.Second
+
 func handleRemoveTriggersStartWith(logger moira.Logger, database moira.Database, prefix string) error {
 	triggers, err := database.GetTriggerIDsStartWith(prefix)
 	if err != nil {
 		return fmt.Errorf("can't get trigger IDs start with prefix %s: %w", prefix, err)
 	}
 
-	// Added delay because command is potentially dangerous and can delete unwanted triggers
-	const delay = 10 * time.Second
+	return deleteTriggers(logger, triggers, prefix, database)
+}
+
+func handleRemoveUnusedTriggersStartWith(logger moira.Logger, database moira.Database, prefix string) error {
+	triggers, err := database.GetTriggerIDsStartWith(prefix)
+	if err != nil {
+		return fmt.Errorf("can't get trigger IDs start with prefix %s: %w", prefix, err)
+	}
+	unusedTriggers, err := database.GetUnusedTriggerIDs()
+	if err != nil {
+		return fmt.Errorf("can't get unused trigger IDs; err: %w", err)
+	}
+	unusedTriggersMap := map[string]bool{}
+	for _, id := range unusedTriggers {
+		unusedTriggersMap[id] = true
+	}
+
+	triggersToDelete := make([]string, 0)
+	for _, id := range triggers {
+		if unusedTriggersMap[id] {
+			triggersToDelete = append(triggersToDelete, id)
+		}
+	}
+
+	return deleteTriggers(logger, triggersToDelete, prefix, database)
+}
+
+func deleteTriggers(logger moira.Logger, triggers []string, prefix string, database moira.Database) error {
 	logger.Info().
 		Int("triggers_to_delete", len(triggers)).
 		String("prefix", prefix).
@@ -27,11 +56,13 @@ func handleRemoveTriggersStartWith(logger moira.Logger, database moira.Database,
 		String("prefix", prefix).
 		Msg("Removing triggers start with given prefix has started")
 
+	deletedTriggersCount := 0
 	for _, id := range triggers {
 		err := database.RemoveTrigger(id)
 		if err != nil {
 			return fmt.Errorf("can't remove trigger with id %s: %w", id, err)
 		}
+		deletedTriggersCount++
 	}
 	logger.Info().
 		String("prefix", prefix).
