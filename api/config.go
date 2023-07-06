61f4f01ffa44e3002f7a5782a96cc40db2148c3b
Xenia N <nisskhen.kd@skbkontur.ru>
2023-07-06 10:46:25 +0600

    feat(api): add feature-flag into /config (#870)

--- api/config.go
+++ api/config.go
@@ -2,7 +2,7 @@ package api
 
 import "time"
 
-// Config for api configuration variables
+// Config for api configuration variables.
 type Config struct {
 	EnableCORS      bool
 	Listen          string
@@ -10,18 +10,26 @@ type Config struct {
 	RemoteMetricTTL time.Duration
 }
 
-// WebConfig is container for web ui configuration parameters
+// WebConfig is container for web ui configuration parameters.
 type WebConfig struct {
 	SupportEmail  string       `json:"supportEmail,omitempty"`
 	RemoteAllowed bool         `json:"remoteAllowed"`
 	Contacts      []WebContact `json:"contacts"`
+	FeatureFlags  FeatureFlags `json:"featureFlags"`
 }
 
-// WebContact is container for web ui contact validation
+// WebContact is container for web ui contact validation.
 type WebContact struct {
 	ContactType     string `json:"type"`
 	ContactLabel    string `json:"label"`
 	ValidationRegex string `json:"validation,omitempty"`
 	Placeholder     string `json:"placeholder,omitempty"`
 	Help            string `json:"help,omitempty"`
+}
+
+// FeatureFlags is struct to manage feature flags.
+type FeatureFlags struct {
+	IsPlottingDefaultOn              bool `json:"isPlottingDefaultOn"`
+	IsPlottingAvailable              bool `json:"isPlottingAvailable"`
+	IsSubscriptionToAllTagsAvailable bool `json:"isSubscriptionToAllTagsAvailable"`
 }
