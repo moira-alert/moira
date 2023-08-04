28dc5b21308fda3175849d2390727fcfdb2e4ac5
almostinf <almostinf@mail.ru>
2023-08-04 16:12:20 +0300

    add tests for handler

---
+++ api/handler/trigger_metric_test.go
@@ -0,0 +1,64 @@
+package handler
+
+import (
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/moira-alert/moira"
+	"github.com/moira-alert/moira/api/middleware"
+	mock_moira_alert "github.com/moira-alert/moira/mock/moira-alert"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestDeleteTriggerMetric(t *testing.T) {
+	Convey("Delete metric by name", t, func() {
+		mockCtrl := gomock.NewController(t)
+		defer mockCtrl.Finish()
+		responseWriter := httptest.NewRecorder()
+		mockDb := mock_moira_alert.NewMockDatabase(mockCtrl)
+
+		Convey("with the correct name", func() {
+			mockDb.EXPECT().GetTrigger("triggerID-0000000000001").Return(moira.Trigger{ID: "triggerID-0000000000001"}, nil)
+			mockDb.EXPECT().AcquireTriggerCheckLock(gomock.Any(), gomock.Any()).Return(nil)
+			mockDb.EXPECT().GetTriggerLastCheck(gomock.Any()).Return(moira.CheckData{}, nil)
+			mockDb.EXPECT().DeleteTriggerCheckLock(gomock.Any()).Return(nil)
+			mockDb.EXPECT().RemovePatternsMetrics(gomock.Any()).Return(nil)
+			mockDb.EXPECT().SetTriggerLastCheck(gomock.Any(), gomock.Any(), gomock.Any()).Return(nil)
+			database = mockDb
+
+			testRequest := httptest.NewRequest(http.MethodDelete, "/trigger/triggerID-0000000000001/metrics?name=test", nil)
+			testRequest = testRequest.WithContext(middleware.SetContextValueForTest(testRequest.Context(), "triggerID", "triggerID-0000000000001"))
+
+			deleteTriggerMetric(responseWriter, testRequest)
+
+			response := responseWriter.Result()
+			defer response.Body.Close()
+			contentBytes, _ := io.ReadAll(response.Body)
+			contents := string(contentBytes)
+
+			So(contents, ShouldEqual, "")
+			So(response.StatusCode, ShouldEqual, http.StatusOK)
+		})
+
+		Convey("with wrong name", func() {
+			testRequest := httptest.NewRequest(http.MethodDelete, "/trigger/triggerID-0000000000001/metrics?name=test%name", nil)
+			testRequest = testRequest.WithContext(middleware.SetContextValueForTest(testRequest.Context(), "triggerID", "triggerID-0000000000001"))
+			testRequest.Header.Add("content-type", "application/json")
+
+			deleteTriggerMetric(responseWriter, testRequest)
+
+			response := responseWriter.Result()
+			defer response.Body.Close()
+			contentBytes, _ := io.ReadAll(response.Body)
+			contents := string(contentBytes)
+			expected := `{"status":"Invalid request","error":"invalid URL escape \"%na\""}
+`
+
+			So(contents, ShouldEqual, expected)
+			So(response.StatusCode, ShouldEqual, http.StatusBadRequest)
+		})
+	})
+}
