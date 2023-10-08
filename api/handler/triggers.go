d1b37a318c53a7e1518d9ec536f59efb8c00eb01
almostinf <almostinf@mail.ru>
2023-10-08 21:21:39 +0300

    fix delete pager swaggo doc

--- api/handler/triggers.go
+++ api/handler/triggers.go
@@ -288,10 +288,10 @@ func searchTriggers(writer http.ResponseWriter, request *http.Request) {
 // nolint: gofmt,goimports
 //
 //	@summary	Delete triggers pager
-//	@id				delete-pager
+//	@id			delete-pager
 //	@tags		trigger
 //	@produce	json
-//	@param			pagerID			query		string							false	"Pager ID"				default(bcba82f5-48cf-44c0-b7d6-e1d32c64a88c)								true	"Pager ID to delete"	default(bcba82f5-48cf-44c0-b7d6-e1d32c64a88c)
+//	@param		pagerID	query		string									false	"Pager ID"	default(bcba82f5-48cf-44c0-b7d6-e1d32c64a88c)	true	"Pager ID to delete"	default(bcba82f5-48cf-44c0-b7d6-e1d32c64a88c)
 //	@success	200		{object}	dto.TriggersSearchResultDeleteResponse	"Successfully deleted pager"
 //	@failure	404		{object}	api.ErrorNotFoundExample				"Resource not found"
 //	@failure	422		{object}	api.ErrorRenderExample					"Render error"
