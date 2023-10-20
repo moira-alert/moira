c761419ad34094b5d4d380839849864147f58706
almostinf <almostinf@mail.ru>
2023-10-21 00:54:13 +0300

    add tests for contact handler

--- api/handler/contact.go
+++ api/handler/contact.go
@@ -100,7 +100,7 @@ func createNewContact(writer http.ResponseWriter, request *http.Request) {
 	}
 	userLogin := middleware.GetLogin(request)
 
-	if err := controller.CreateContact(database, contact, userLogin, ""); err != nil {
+	if err := controller.CreateContact(database, contact, userLogin, contact.TeamID); err != nil {
 		render.Render(writer, request, err) //nolint
 		return
 	}
