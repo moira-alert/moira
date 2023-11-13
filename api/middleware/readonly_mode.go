30ca6fbb89ee8540bedf84a679446f89505204fc
Tetrergeru <ArutunoffOleg@yandex.ru>
2023-11-13 13:55:30 +0100

    Fix lint

--- api/middleware/readonly_mode.go
+++ api/middleware/readonly_mode.go
@@ -11,7 +11,7 @@ func ReadOnlyMiddleware(config *api.Config) func(next http.Handler) http.Handler
 	return func(next http.Handler) http.Handler {
 		fn := func(w http.ResponseWriter, r *http.Request) {
 			if config.Flags.IsReadonlyEnabled && IsMutatingMethod(r.Method) {
-				render.Render(w, r, api.ErrorForbidden("Moira is currently in read-only mode"))
+				render.Render(w, r, api.ErrorForbidden("Moira is currently in read-only mode")) //nolint:errcheck
 				return
 			}
 			next.ServeHTTP(w, r)
