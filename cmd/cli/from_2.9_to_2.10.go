0633491fd4bcefe13f25d43ab667db433cb0b1fe
Tetrergeru <ArutunoffOleg@yandex.ru>
2023-11-22 11:29:28 +0100

    hotfix(cli): Fix add key for local triggers (#969)

--- cmd/cli/from_2.9_to_2.10.go
+++ cmd/cli/from_2.9_to_2.10.go
@@ -43,16 +43,16 @@ func createKeyForLocalTriggers(ctx context.Context, logger moira.Logger, databas
 
 	switch d := database.(type) {
 	case *redis.DbConnector:
-		pipe := d.Client().TxPipeline()
+		client := d.Client()
 
-		localTriggerIds, err := pipe.SDiff(ctx, triggersListKey, remoteTriggersListKey, prometheusTriggersListKey).Result()
+		localTriggerIds, err := client.SDiff(ctx, triggersListKey, remoteTriggersListKey, prometheusTriggersListKey).Result()
 		if err != nil {
 			return err
 		}
 
 		logger.Info().Msg("Finish getting local trigger IDs")
 
-		_, err = pipe.SAdd(ctx, localTriggersListKey, localTriggerIds).Result()
+		_, err = client.SAdd(ctx, localTriggersListKey, localTriggerIds).Result()
 		if err != nil {
 			return err
 		}
@@ -70,9 +70,9 @@ func revertCreateKeyForLocalTriggers(ctx context.Context, logger moira.Logger, d
 
 	switch d := database.(type) {
 	case *redis.DbConnector:
-		pipe := d.Client().TxPipeline()
+		client := d.Client()
 
-		_, err := pipe.Del(ctx, localTriggersListKey).Result()
+		err := client.Del(ctx, localTriggersListKey).Err()
 		if err != nil {
 			return err
 		}
