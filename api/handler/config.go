5f6f01d49cf0040211bfcd348d234399d6da0763
almostinf <almostinf@mail.ru>
2023-12-22 15:33:47 +0300

    swag fmt

--- api/handler/config.go
+++ api/handler/config.go
@@ -13,8 +13,8 @@ import (
 //	@id			get-web-config
 //	@tags		config
 //	@produce	json
-//	@success	200	{object}	api.WebConfig	"Configuration fetched successfully"
-//	@failure	422	{object}	api.ErrorRenderExample			"Render error"
+//	@success	200	{object}	api.WebConfig			"Configuration fetched successfully"
+//	@failure	422	{object}	api.ErrorRenderExample	"Render error"
 //	@router		/config [get]
 func getWebConfig(webConfig *api.WebConfig) http.HandlerFunc {
 	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
