27ae19b7a13994e2ab0c56d00f6b970817a204fe
almostinf <almostinf@mail.ru>
2023-12-28 14:31:34 +0300

    update split strings character

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -327,7 +327,7 @@ func main() { //nolint
 
 	if *removeSubscriptions != "" {
 		logger.Info().Msg("Start delition of subscriptions")
-		subscriptionIDs := strings.Split(*removeSubscriptions, ",")
+		subscriptionIDs := strings.Split(*removeSubscriptions, " ")
 		logger.Info().
 			String("command", *removeSubscriptions).
 			Interface("subscription_ids", subscriptionIDs).
