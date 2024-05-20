9c685e956e6ca7852ea9de39cf9580bcd5f4ae75
almostinf <almostinf@mail.ru>
2024-05-20 16:56:09 +0300

    fix linter changes

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -456,7 +456,7 @@ func openFile(filePath string, mode int) (*os.File, error) {
 	if filePath == "" {
 		return nil, fmt.Errorf("file is not specified")
 	}
-	file, err := os.OpenFile(filePath, mode, 0o666) //nolint:gofumpt,gomnd
+	file, err := os.OpenFile(filePath, mode, 0666) //nolint:gofumpt,gomnd
 	if err != nil {
 		return nil, fmt.Errorf("cannot open file: %w", err)
 	}
