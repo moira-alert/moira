fa50c3d1a2ee5d34af95ed666c9219de70e0b0db
gaspero <gasp-gasp@mail.ru>
2024-05-26 23:21:19 +0300

    fix(cli): Add v2.11 to moiraValidVersions
    Relates #1009

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -23,7 +23,7 @@ var (
 	GoVersion    = "unknown"
 )
 
-var moiraValidVersions = []string{"2.3", "2.6", "2.7", "2.9"}
+var moiraValidVersions = []string{"2.3", "2.6", "2.7", "2.9", "2.11"}
 
 var (
 	configFileName         = flag.String("config", "/etc/moira/cli.yml", "Path to configuration file")
