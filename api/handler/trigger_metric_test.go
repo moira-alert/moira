ae1da43cc4924bdd3e578bff89f43fe1f9fd3829
Danil Tarasov <87192879+almostinf@users.noreply.github.com>
2024-07-17 11:11:27 +0300

    feat(notifier): support group topics in Telegram sender (#1044)
    Authored-by: Ivan Shadrin (@Gaspero)

--- api/handler/trigger_metric_test.go
+++ api/handler/trigger_metric_test.go
@@ -6,11 +6,11 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/golang/mock/gomock"
 	"github.com/moira-alert/moira"
 	"github.com/moira-alert/moira/api/middleware"
 	mock_moira_alert "github.com/moira-alert/moira/mock/moira-alert"
 	. "github.com/smartystreets/goconvey/convey"
+	"go.uber.org/mock/gomock"
 )
 
 func TestDeleteTriggerMetric(t *testing.T) {
