887cab5f5867d85f9a235015065d4e941b4bcd9d
almostinf <almostinf@mail.ru>
2024-07-18 11:26:14 +0300

    fix notifier logs in user and team contact

--- notifier/log_common.go
+++ notifier/log_common.go
@@ -8,16 +8,26 @@ func getLogWithPackageContext(log *moira.Logger, pkg *NotificationPackage, confi
 		String(moira.LogFieldNameContactType, pkg.Contact.Type).
 		String(moira.LogFieldNameContactValue, pkg.Contact.Value).
 		Int(moira.LogFieldNameFailCount, pkg.FailCount).
-		String(moira.LogFieldNameContactUser, pkg.Contact.User).
-		String(moira.LogFieldNameContactTeam, pkg.Contact.Team).
 		String(moira.LogFieldNameContext, "notification")
+
 	if pkg.Trigger.ID != "" { // note: test notification without trigger info
 		logger.
 			String(moira.LogFieldNameTriggerID, pkg.Trigger.ID).
 			String(moira.LogFieldNameTriggerName, pkg.Trigger.Name)
 	}
 
+	if pkg.Contact.User != "" {
+		logger.
+			String(moira.LogFieldNameContactUser, pkg.Contact.User)
+	}
+
+	if pkg.Contact.Team != "" {
+		logger.
+			String(moira.LogFieldNameContactTeam, pkg.Contact.Team)
+	}
+
 	SetLogLevelByConfig(config.LogContactsToLevel, pkg.Contact.ID, &logger)
+
 	return logger
 }
 
