1f95f7aba2fc5b03bfba8c0b384e5b66e849d125
AleksandrMatsko <AMatsko54@gmail.com>
2024-07-29 15:17:30 +0700

    style: remove comments for default values from api

--- api/handler/constants.go
+++ api/handler/constants.go
@@ -1,12 +1,8 @@
 package handler
 
-// default values for contactEvents router.
 const (
-	// default values for middleware.DateRange.
 	contactEventsDefaultFrom = "-3hour"
 	contactEventsDefaultTo   = "now"
-
-	// default values for middleware.Paginate.
 	contactEventsDefaultPage = 0
 	contactEventsDefaultSize = -1
 )
