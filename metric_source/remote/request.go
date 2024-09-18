8c52507561cb7933ed005ed1b7afe513324dc8bd
AleksandrMatsko <AMatsko54@gmail.com>
2024-09-18 15:02:25 +0700

    refactor: remove comments, add new lines

--- metric_source/remote/request.go
+++ metric_source/remote/request.go
@@ -14,43 +14,20 @@ func (remote *Remote) prepareRequest(from, until int64, target string) (*http.Re
 	if err != nil {
 		return nil, err
 	}
+
 	q := req.URL.Query()
 	q.Add("format", "json")
 	q.Add("from", strconv.FormatInt(from, 10))
 	q.Add("target", target)
 	q.Add("until", strconv.FormatInt(until, 10))
 	req.URL.RawQuery = q.Encode()
+
 	if remote.config.User != "" && remote.config.Password != "" {
 		req.SetBasicAuth(remote.config.User, remote.config.Password)
 	}
+
 	return req, nil
 }
-
-// Old make request
-//func (remote *Remote) makeRequest(req *http.Request) ([]byte, error) {
-//	var body []byte
-//
-//	resp, err := remote.client.Do(req)
-//	if resp != nil {
-//		defer resp.Body.Close()
-//	}
-//
-//	if err != nil {
-//		return body, fmt.Errorf("The remote server is not available or the response was reset by timeout. "+ //nolint
-//			"TTL: %s, PATH: %s, ERROR: %v ", remote.client.Timeout.String(), req.URL.RawPath, err)
-//	}
-//
-//	body, err = io.ReadAll(resp.Body)
-//	if err != nil {
-//		return body, err
-//	}
-//
-//	if resp.StatusCode != http.StatusOK {
-//		return body, fmt.Errorf("bad response status %d: %s", resp.StatusCode, string(body))
-//	}
-//
-//	return body, nil
-//}
 
 func (remote *Remote) makeRequest(req *http.Request) (body []byte, isRemoteAvailable bool, err error) {
 	resp, err := remote.client.Do(req)
@@ -73,8 +50,8 @@ func (remote *Remote) makeRequest(req *http.Request) (body []byte, isRemoteAvail
 
 	if isRemoteUnavailableStatusCode(resp.StatusCode) {
 		return body, false, fmt.Errorf(
-			"the remote server is not available. Response status %d: %s", resp.StatusCode, string(body),
-		)
+			"the remote server is not available. Response status %d: %s",
+			resp.StatusCode, string(body))
 	} else if resp.StatusCode != http.StatusOK {
 		return body, true, fmt.Errorf("bad response status %d: %s", resp.StatusCode, string(body))
 	}
