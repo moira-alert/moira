65419292efbfaaf8c2369399d860fb3f56b089f9
Aleksandr Matsko <90016901+AleksandrMatsko@users.noreply.github.com>
2024-10-15 14:58:02 +0700

    feat: trigger name limit (#1080)

--- cmd/api/config.go
+++ cmd/api/config.go
@@ -48,6 +48,29 @@ type apiConfig struct {
 	EnableCORS bool `yaml:"enable_cors"`
 	// Authorization contains authorization configuration.
 	Authorization authorization `yaml:"authorization"`
+	// Limits contains limits applied to entities and so on.
+	Limits LimitsConfig `yaml:"limits"`
+}
+
+// LimitsConfig contains configurable moira limits.
+type LimitsConfig struct {
+	// Trigger contains the limits applied to triggers.
+	Trigger TriggerLimitsConfig `yaml:"trigger"`
+}
+
+// TriggerLimitsConfig represents the limits which will be applied to all triggers.
+type TriggerLimitsConfig struct {
+	// MaxNameSize is the max amount of characters allowed in trigger name.
+	MaxNameSize int `yaml:"max_name_size"`
+}
+
+// ToLimits converts LimitsConfig to api.LimitsConfig.
+func (conf LimitsConfig) ToLimits() api.LimitsConfig {
+	return api.LimitsConfig{
+		Trigger: api.TriggerLimits{
+			MaxNameSize: conf.Trigger.MaxNameSize,
+		},
+	}
 }
 
 type authorization struct {
@@ -116,6 +139,7 @@ func (config *apiConfig) getSettings(
 		MetricsTTL:    metricsTTL,
 		Flags:         flags,
 		Authorization: config.Authorization.toApiConfig(webConfig),
+		Limits:        config.Limits.ToLimits(),
 	}
 }
 
@@ -231,6 +255,11 @@ func getDefault() config {
 		API: apiConfig{
 			Listen:     ":8081",
 			EnableCORS: false,
+			Limits: LimitsConfig{
+				Trigger: TriggerLimitsConfig{
+					MaxNameSize: api.DefaultTriggerNameMaxSize,
+				},
+			},
 		},
 		Web: webConfig{
 			RemoteAllowed: false,
