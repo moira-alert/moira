e5b05f40a04b481b5236a107af3843437327ff4f
AleksandrMatsko <AMatsko54@gmail.com>
2025-02-28 12:28:47 +0700

    feat: add metric delivery_checks_stopped to sender metrics

--- metrics/sender.go
+++ metrics/sender.go
@@ -2,8 +2,9 @@ package metrics
 
 // SenderMetrics should be used for sender which can understand if the notification was delivered or not.
 type SenderMetrics struct {
-	ContactDeliveryNotificationOK     Meter
-	ContactDeliveryNotificationFailed Meter
+	ContactDeliveryNotificationOK           Meter
+	ContactDeliveryNotificationFailed       Meter
+	ContactDeliveryNotificationCheckStopped Meter
 }
 
 // ConfigureSenderMetrics configures SenderMetrics using NotifierMetrics with given graphiteIdent for senderContactType.
@@ -13,5 +14,7 @@ func ConfigureSenderMetrics(notifierMetrics *NotifierMetrics, graphiteIdent stri
 			RegisterMeter(senderContactType, graphiteIdent, "delivery_ok"),
 		ContactDeliveryNotificationFailed: notifierMetrics.ContactsDeliveryNotificationsFailed.
 			RegisterMeter(senderContactType, graphiteIdent, "delivery_failed"),
+		ContactDeliveryNotificationCheckStopped: notifierMetrics.ContactsDeliveryNotificationsChecksStopped.
+			RegisterMeter(senderContactType, graphiteIdent, "delivery_checks_stopped"),
 	}
 }
