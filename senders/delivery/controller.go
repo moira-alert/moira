66321a4aba4adf8f38b73f1c2359aff3d97434b8
AleksandrMatsko <AMatsko54@gmail.com>
2025-03-13 20:17:02 +0700

    feat: delivery checks database controller

---
+++ senders/delivery/controller.go
@@ -0,0 +1,80 @@
+package delivery
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/moira-alert/moira"
+	"github.com/moira-alert/moira/database"
+	"github.com/moira-alert/moira/metrics"
+)
+
+type ChecksController struct {
+	database    moira.DeliveryCheckerDatabase
+	lock        moira.Lock
+	contactType string
+}
+
+func NewChecksController(db moira.DeliveryCheckerDatabase, lock moira.Lock, contactType string) *ChecksController {
+	return &ChecksController{
+		database:    db,
+		lock:        lock,
+		contactType: contactType,
+	}
+}
+
+func (controller *ChecksController) AddDeliveryChecksData(timestamp int64, data string) error {
+	return controller.database.AddDeliveryChecksData(controller.contactType, timestamp, data)
+}
+
+func (controller *ChecksController) addManyDeliveryChecksData(timestamp int64, data []string) error {
+	for _, singleData := range data {
+		err := controller.database.AddDeliveryChecksData(controller.contactType, timestamp, singleData)
+		if err != nil {
+			return fmt.Errorf("failed to store check data: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func (controller *ChecksController) getDeliveryChecksData(from string, to string) ([]string, error) {
+	marshaledData, err := controller.database.GetDeliveryChecksData(controller.contactType, from, to)
+	if err != nil {
+		if errors.Is(err, database.ErrNil) {
+			// nothing to check
+			return nil, nil
+		}
+
+		return nil, err
+	}
+
+	return marshaledData, nil
+}
+
+func (controller *ChecksController) removeDeliveryChecksData(from string, to string) error {
+	_, err := controller.database.RemoveDeliveryChecksData(controller.contactType, from, to)
+	return err
+}
+
+func (controller *ChecksController) RunDeliveryChecksWorker(
+	stop <-chan struct{},
+	logger moira.Logger,
+	checkTimeout time.Duration,
+	reschedulingDelay uint64,
+	metrics *metrics.SenderMetrics,
+	checkAction CheckAction,
+) {
+	checkWorker := newChecksWorker(
+		logger,
+		workerNameSuffix+" "+controller.contactType,
+		checkTimeout,
+		reschedulingDelay,
+		controller,
+		metrics,
+		checkAction,
+	)
+
+	go checkWorker.run(controller.lock, stop)
+}
---
+++ senders/delivery/worker.go
@@ -0,0 +1,130 @@
+package delivery
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/moira-alert/moira"
+	"github.com/moira-alert/moira/clock"
+	"github.com/moira-alert/moira/metrics"
+	"github.com/moira-alert/moira/worker"
+)
+
+const (
+	workerNameSuffix                    = "DeliveryChecker"
+	logFieldNameDeliveryCheckWorkerName = "delivery.check.worker.name"
+)
+
+type checksStorage interface {
+	addManyDeliveryChecksData(timestamp int64, data []string) error
+	getDeliveryChecksData(from string, to string) ([]string, error)
+	removeDeliveryChecksData(from string, to string) error
+}
+
+type checksWorker struct {
+	logger            moira.Logger
+	clock             moira.Clock
+	workerName        string
+	checkTimeout      time.Duration
+	reschedulingDelay uint64
+	storage           checksStorage
+	metrics           *metrics.SenderMetrics
+	checkAction       CheckAction
+}
+
+func newChecksWorker(
+	logger moira.Logger,
+	workerName string,
+	checkTimeout time.Duration,
+	reschedulingDelay uint64,
+	storage checksStorage,
+	metrics *metrics.SenderMetrics,
+	checkAction CheckAction,
+) *checksWorker {
+	logger = logger.String(logFieldNameDeliveryCheckWorkerName, workerName)
+
+	return &checksWorker{
+		logger:            logger,
+		clock:             clock.NewSystemClock(),
+		workerName:        workerName,
+		checkTimeout:      checkTimeout,
+		reschedulingDelay: reschedulingDelay,
+		storage:           storage,
+		metrics:           metrics,
+		checkAction:       checkAction,
+	}
+}
+
+func (checksWorker *checksWorker) run(lock moira.Lock, stop <-chan struct{}) {
+	worker.NewWorker(
+		checksWorker.workerName,
+		checksWorker.logger,
+		lock,
+		checksWorker.deliveryCheckerAction,
+	).Run(stop)
+}
+
+func (checksWorker *checksWorker) deliveryCheckerAction(stop <-chan struct{}) error {
+	checkTicker := time.NewTicker(checksWorker.checkTimeout)
+
+	checksWorker.logger.Info().Msg(checksWorker.workerName + " started")
+	for {
+		select {
+		case <-stop:
+			checksWorker.logger.Info().Msg(checksWorker.workerName + " stopped")
+			checkTicker.Stop()
+			return nil
+
+		case <-checkTicker.C:
+			if err := checksWorker.checkNotificationsDelivery(); err != nil {
+				checksWorker.logger.Error().
+					Error(err).
+					Msg("failed to perform delivery check")
+			}
+		}
+	}
+}
+
+func (checksWorker *checksWorker) checkNotificationsDelivery() error {
+	fetchTimestamp := checksWorker.clock.NowUnix()
+
+	marshaledData, err := checksWorker.storage.getDeliveryChecksData("-inf", strconv.FormatInt(fetchTimestamp, 10))
+	if err != nil {
+		return err
+	}
+
+	if len(marshaledData) == 0 {
+		return nil
+	}
+
+	counter := TypesCounter{}
+
+	checkAgainChecksData := checksWorker.checkAction.CheckNotificationsDelivery(marshaledData, &counter)
+
+	err = checksWorker.storage.addManyDeliveryChecksData(checksWorker.clock.NowUnix()+int64(checksWorker.reschedulingDelay), checkAgainChecksData)
+	if err != nil {
+		return fmt.Errorf("failed to reschedule delivery checks: %w", err)
+	}
+
+	err = checksWorker.storage.removeDeliveryChecksData("-inf", strconv.FormatInt(fetchTimestamp, 10))
+	if err != nil {
+		checksWorker.logger.Warning().
+			Error(err).
+			Msg("failed to remove outdated delivery checks")
+	}
+
+	markMetrics(checksWorker.metrics, &counter)
+
+	return nil
+}
+
+func markMetrics(senderMetrics *metrics.SenderMetrics, counter *TypesCounter) {
+	if senderMetrics == nil || counter == nil {
+		return
+	}
+
+	senderMetrics.ContactDeliveryNotificationOK.Mark(counter.DeliveryOK)
+	senderMetrics.ContactDeliveryNotificationFailed.Mark(counter.DeliveryFailed)
+	senderMetrics.ContactDeliveryNotificationCheckStopped.Mark(counter.DeliveryChecksStopped)
+}
