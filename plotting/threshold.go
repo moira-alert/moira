fe6585008288aada90da0ae88bd6fb23de0c47d9
Xenia Nisskhen <nisskhen.kd@skbkontur.ru>
2025-04-03 19:47:34 +0500

    feat(linter): Add wsl linter into plotting|logging|worker

--- plotting/threshold.go
+++ plotting/threshold.go
@@ -50,7 +50,7 @@ func getThresholdSeriesList(trigger *moira.Trigger, theme moira.PlotTheme, limit
 		thresholdSeriesList = append(thresholdSeriesList, plotThreshold.generateThresholdSeries(theme, limits))
 		// TODO: uncomment to use annotations if necessary, remove otherwise
 		// thresholdSeriesList = append(thresholdSeriesList, plotThreshold.generateAnnotationSeries(theme, limits))
-	}
+	} //nolint:wsl
 
 	return thresholdSeriesList
 }
