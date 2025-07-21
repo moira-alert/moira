2997b47d022c93888459c9b37aa14259f965beab
Tetrergeru <ArutunoffOleg@yandex.ru>
2025-07-21 08:59:03 +0200

    fix: Extra Message in team settings (#1212)

--- api/dto/team.go
+++ api/dto/team.go
@@ -126,6 +126,20 @@ type TeamContact struct {
 	ExtraMessage string `json:"extra_message,omitempty"`
 }
 
+// MakeTeamContact converts moira.ContactData to a TeamContact.
+func MakeTeamContact(contact *moira.ContactData) TeamContact {
+	return TeamContact{
+		ID:           contact.ID,
+		Name:         contact.Name,
+		User:         contact.User,
+		TeamID:       contact.Team,
+		Team:         contact.Team,
+		Type:         contact.Type,
+		Value:        contact.Value,
+		ExtraMessage: contact.ExtraMessage,
+	}
+}
+
 // Render is a function that implements chi Renderer interface for TeamContact.
 func (TeamContact) Render(w http.ResponseWriter, r *http.Request) error {
 	return nil
