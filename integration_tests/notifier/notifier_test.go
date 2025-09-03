2ebf7a264166fa1a491580a34f1da46baf34e2e1
HeavyPunk <kirillkuchma8@gmail.com>
2025-09-03 18:02:41 +0500

    feat: write new metrics for notifier

--- integration_tests/notifier/notifier_test.go
+++ integration_tests/notifier/notifier_test.go
@@ -1,6 +1,7 @@
 package notifier
 
 import (
+	"context"
 	"fmt"
 	"testing"
 	"time"
@@ -43,9 +44,9 @@ var notifierConfig = notifier.Config{
 var shutdown = make(chan struct{})
 
 var (
-	notifierMetrics = metrics.ConfigureNotifierMetrics(metrics.NewDummyRegistry(), "notifier")
-	logger, _       = logging.GetLogger("Notifier_Test")
-	mockCtrl        *gomock.Controller
+	notifierMetrics, _ = metrics.ConfigureNotifierMetrics(metrics.NewDummyRegistry(), metrics.NewMetricContext(context.Background()).CreateRegistry(), "notifier")
+	logger, _          = logging.GetLogger("Notifier_Test")
+	mockCtrl           *gomock.Controller
 )
 
 var contact = moira.ContactData{
--- metrics/sender.go
+++ metrics/sender.go
@@ -8,13 +8,42 @@ type SenderMetrics struct {
 }
 
 // ConfigureSenderMetrics configures SenderMetrics using NotifierMetrics with given graphiteIdent for senderContactType.
-func ConfigureSenderMetrics(notifierMetrics *NotifierMetrics, graphiteIdent string, senderContactType string) *SenderMetrics {
-	return &SenderMetrics{
-		ContactDeliveryNotificationOK: notifierMetrics.ContactsDeliveryNotificationsOK.
-			RegisterMeter(senderContactType, graphiteIdent, "delivery_ok"),
-		ContactDeliveryNotificationFailed: notifierMetrics.ContactsDeliveryNotificationsFailed.
-			RegisterMeter(senderContactType, graphiteIdent, "delivery_failed"),
-		ContactDeliveryNotificationCheckStopped: notifierMetrics.ContactsDeliveryNotificationsChecksStopped.
-			RegisterMeter(senderContactType, graphiteIdent, "delivery_checks_stopped"),
+func ConfigureSenderMetrics(notifierMetrics *NotifierMetrics, graphiteIdent string, senderContactType string) (*SenderMetrics, error) {
+	const senderContactTypeField = "sender_contact_type"
+
+	deliveryOk, err := notifierMetrics.ContactsDeliveryNotificationsOKAttributed.RegisterMeter("delivery_ok", Attributes{
+		Attribute{Key: senderContactTypeField, Value: graphiteIdent},
+	})
+	if err != nil {
+		return nil, err
 	}
+
+	deliveryFailed, err := notifierMetrics.ContactsDeliveryNotificationsFailedAttributed.RegisterMeter("delivery_failed", Attributes{
+		Attribute{Key: senderContactTypeField, Value: graphiteIdent},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	deliveryChecksStopped, err := notifierMetrics.ContactsDeliveryNotificationsChecksStoppedAttributed.RegisterMeter("delivery_checks_stopped", Attributes{
+		Attribute{Key: senderContactTypeField, Value: graphiteIdent},
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &SenderMetrics{
+		ContactDeliveryNotificationOK: NewCompositeMeter(
+			notifierMetrics.ContactsDeliveryNotificationsOK.RegisterMeter(senderContactType, graphiteIdent, "delivery_ok"),
+			deliveryOk,
+		),
+		ContactDeliveryNotificationFailed: NewCompositeMeter(
+			notifierMetrics.ContactsDeliveryNotificationsFailed.RegisterMeter(senderContactType, graphiteIdent, "delivery_failed"),
+			deliveryFailed,
+		),
+		ContactDeliveryNotificationCheckStopped: NewCompositeMeter(
+			notifierMetrics.ContactsDeliveryNotificationsChecksStopped.RegisterMeter(senderContactType, graphiteIdent, "delivery_checks_stopped"),
+			deliveryChecksStopped,
+		),
+	}, nil
 }
