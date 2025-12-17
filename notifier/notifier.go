436c4654d2bff752d2706fadc02cf20ce15e1b9e
Xenia Nisskhen <nisskhen.kd@skbkontur.ru>
2025-12-17 10:38:25 +0500

    fix(notifier|linter) Fix skip render error and upgrade linter

--- notifier/notifier.go
+++ notifier/notifier.go
@@ -1,11 +1,13 @@
 package notifier
 
 import (
+	"errors"
 	"fmt"
 	"runtime/debug"
 	"sync"
 	"time"
 
+	"github.com/moira-alert/go-chart"
 	"github.com/moira-alert/moira"
 	"github.com/moira-alert/moira/logging"
 	metricSource "github.com/moira-alert/moira/metric_source"
@@ -214,12 +216,16 @@ func (notifier *StandardNotifier) runSender(sender moira.Sender, ch chan Notific
 
 		plots, plotsBuildDuration, plotsBuildErr := notifier.buildNotificationPackagePlots(pkg, log)
 		if plotsBuildErr != nil {
-			var event logging.EventBuilder
+			var (
+				event      logging.EventBuilder
+				errNoPoint plotting.ErrNoPointsToRender
+			)
 
-			switch plotsBuildErr.(type) { // nolint:errorlint
-			case plotting.ErrNoPointsToRender:
+			if errors.Is(plotsBuildErr, chart.ErrOnePoint) {
+				event = log.Warning()
+			} else if errors.As(plotsBuildErr, &errNoPoint) {
 				event = log.Debug()
-			default:
+			} else {
 				event = log.Error()
 			}
 
