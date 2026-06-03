75c9e92477334f5e4a9bba560077255c624398c5
Xenia Nisskhen <nisskhen.kd@skbkontur.ru>
2026-06-03 12:08:52 +0500

    fix(api) Add validation for admins

--- api/controller/contact_test.go
+++ api/controller/contact_test.go
@@ -453,12 +453,15 @@ func TestAdminsCreatesContact(t *testing.T) {
 		AllowedContactTypes: map[string]struct{}{
 			contactType: {},
 		},
+		AllowedExtraAdminContactTypes: map[string]struct{}{
+			moira.SelfStateSender: {},
+		},
 	}
 
 	contactsTemplate := []api.WebContact{}
 
-	Convey("Create for user", t, func() {
-		Convey("The same user", func() {
+	t.Run("Create for user", func(t *testing.T) {
+		t.Run("The same user", func(t *testing.T) {
 			contact := &dto.Contact{
 				Value: contactValue,
 				Type:  contactType,
@@ -467,11 +470,11 @@ func TestAdminsCreatesContact(t *testing.T) {
 
 			dataBase.EXPECT().SaveContact(gomock.Any()).Return(nil)
 			err := CreateContact(dataBase, auth, contactsTemplate, contact, userLogin, "")
-			So(err, ShouldBeNil)
-			So(contact.User, ShouldResemble, userLogin)
+			require.Nil(t, err)
+			require.Equal(t, userLogin, contact.User)
 		})
 
-		Convey("The same user by admin", func() {
+		t.Run("The same user by admin", func(t *testing.T) {
 			contact := &dto.Contact{
 				Value: contactValue,
 				Type:  contactType,
@@ -480,11 +483,11 @@ func TestAdminsCreatesContact(t *testing.T) {
 
 			dataBase.EXPECT().SaveContact(gomock.Any()).Return(nil)
 			err := CreateContact(dataBase, auth, contactsTemplate, contact, adminLogin, "")
-			So(err, ShouldBeNil)
-			So(contact.User, ShouldResemble, adminLogin)
+			require.Nil(t, err)
+			require.Equal(t, adminLogin, contact.User)
 		})
 
-		Convey("Non admin can not create contact for other user", func() {
+		t.Run("Non admin can not create contact for other user", func(t *testing.T) {
 			contact := &dto.Contact{
 				Value: contactValue,
 				Type:  contactType,
@@ -493,11 +496,11 @@ func TestAdminsCreatesContact(t *testing.T) {
 
 			dataBase.EXPECT().SaveContact(gomock.Any()).Return(nil)
 			err := CreateContact(dataBase, auth, contactsTemplate, contact, userLogin, "")
-			So(err, ShouldBeNil)
-			So(contact.User, ShouldResemble, userLogin)
+			require.Nil(t, err)
+			require.Equal(t, userLogin, contact.User)
 		})
 
-		Convey("Admin can create contact for other user", func() {
+		t.Run("Admin can create contact for other user", func(t *testing.T) {
 			contact := &dto.Contact{
 				Value: contactValue,
 				Type:  contactType,
@@ -506,21 +509,57 @@ func TestAdminsCreatesContact(t *testing.T) {
 
 			dataBase.EXPECT().SaveContact(gomock.Any()).Return(nil)
 			err := CreateContact(dataBase, auth, contactsTemplate, contact, adminLogin, "")
-			So(err, ShouldBeNil)
-			So(contact.User, ShouldResemble, userLogin)
+			require.Nil(t, err)
+			require.Equal(t, userLogin, contact.User)
 		})
 
-		Convey("Admin can create not allowed contact", func() {
+		t.Run("Admin cannot create not allowed contact at all", func(t *testing.T) {
 			contact := &dto.Contact{
 				Value: contactValue,
 				Type:  notAllowedContactType,
 				User:  userLogin,
 			}
 
+			err := CreateContact(dataBase, auth, contactsTemplate, contact, adminLogin, "")
+			require.NotNil(t, err)
+
+			expected := &api.ErrorResponse{
+				Err:            errors.New("cannot create contact with not allowed contact type"),
+				HTTPStatusCode: http.StatusBadRequest,
+				StatusText:     "Invalid request",
+				ErrorText:      "cannot create contact with not allowed contact type",
+			}
+			require.Equal(t, expected, err)
+		})
+
+		t.Run("Admin can create allowed for admins contact", func(t *testing.T) {
+			contact := &dto.Contact{
+				Value: contactValue,
+				Type:  moira.SelfStateSender,
+				User:  userLogin,
+			}
 			dataBase.EXPECT().SaveContact(gomock.Any()).Return(nil)
 			err := CreateContact(dataBase, auth, contactsTemplate, contact, adminLogin, "")
-			So(err, ShouldBeNil)
-			So(contact.User, ShouldResemble, userLogin)
+			require.Nil(t, err)
+			require.Equal(t, userLogin, contact.User)
+		})
+
+		t.Run("User cannot create allowed for admins contact", func(t *testing.T) {
+			contact := &dto.Contact{
+				Value: contactValue,
+				Type:  moira.SelfStateSender,
+				User:  userLogin,
+			}
+			err := CreateContact(dataBase, auth, contactsTemplate, contact, userLogin, "")
+			require.NotNil(t, err)
+
+			expected := &api.ErrorResponse{
+				Err:            errors.New("cannot create contact with not allowed contact type"),
+				HTTPStatusCode: http.StatusBadRequest,
+				StatusText:     "Invalid request",
+				ErrorText:      "cannot create contact with not allowed contact type",
+			}
+			require.Equal(t, expected, err)
 		})
 	})
 }
